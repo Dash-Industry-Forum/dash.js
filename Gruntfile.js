7508e36d15bcb1e6d039c73a6e823aa797ea658b
Kirk Shoop (MS OPEN TECH) <kirk.shoop@microsoft.com>
2013-04-30 17:07:45 -0700

    Add tests for dash objects
    This adds grunt support for running:
    - JsHint (static code check)
    - Jasmine (test spec framework)
    - Istanbul (code coverage analyzer)
    
    Also adds positive tests for the DashParser output and
    DashManifestExtensions functions.
    More tests coming...

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      default_options: {}
+    },
+    watch: {},
+    jshint: {
+      all: [
+        "app/js/*/**/*.js"],
+
+      options: {
+        browser: true,
+        devel: true, // this should be temporary
+        jquery: true,
+        globals: {
+          dijon: true,
+          Q: true,
+          X2JS: true,
+          ObjectIron: true
+        }
+      }
+    },
+    jasmine: {
+      tests: {
+        src: [
+            "app/js/streaming/MediaPlayer.js",
+            "app/js/streaming/Context.js",
+            "app/js/dash/Dash.js",
+            "app/js/dash/DashContext.js",
+            "app/js/*/**/*.js"],
+
+        options: {
+          host: 'http://127.0.0.1:8000/',
+          specs: [
+            'test/js/dash/ParserSuite.js',
+            'test/js/dash/FragmentExtensionsSuite.js',
+            'test/js/dash/DashMetricsExtensionsSuite.js',
+            'test/js/dash/DashManifestExtensionsSuite.js'],
+          vendor: [
+            "app/lib/jquery/js/jquery-1.8.3.min.js",
+            "app/lib/jquery/js/jquery-ui-1.9.2.custom.min.js",
+            "app/lib/q.min.js",
+            "app/lib/xml2json.js",
+            "app/lib/objectiron.js",
+            "app/lib/dijon.js",
+            "test/js/utils/MPDfiles.js"],
+          template : require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            coverage: 'reports/coverage.json',
+            report: 'reports/coverage'},
+          junit: {
+              path: grunt.option('jsunit-path'),
+              consolidate: true}
+        }
+      }
+    }
+  });
+
+  // Require needed grunt-modules
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Define tasks
+  grunt.registerTask('default', ['connect','jshint','jasmine']);
+};
