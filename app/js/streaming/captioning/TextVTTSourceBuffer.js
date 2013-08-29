fa070440edc44c6383088bb5b05a93a41b5bb04c
Dan Sparacio (Akamai) <dsparacio@gmail.com>
2013-08-29 15:34:11 -0700

    Updated license header for Akamai Technologies and renamed method to be inline with new object name.

--- app/js/streaming/captioning/TextVTTSourceBuffer.js
+++ app/js/streaming/captioning/TextVTTSourceBuffer.js
@@ -7,7 +7,7 @@
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  * •  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  * •  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * •  Neither the name of the Digital Primates nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * •  Neither the name of the Akamai Technologies nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -16,8 +16,6 @@ MediaPlayer.dependencies.TextVTTSourceBuffer = function () {
     var video,
         data,
         mimeType;
-
-
 
     return {
         system:undefined,
@@ -36,7 +34,7 @@ MediaPlayer.dependencies.TextVTTSourceBuffer = function () {
                     var label = data.Representation_asArray[0].id,
                         lang = data.lang;
 
-                    self.getTrackHandler().addTextTrack(video, result, label, lang, true).then(
+                    self.getTextTrackExtensions().addTextTrack(video, result, label, lang, true).then(
                         function(track)
                         {
                             eventBus.dispatchEvent({type:"updateend"});
@@ -54,7 +52,7 @@ MediaPlayer.dependencies.TextVTTSourceBuffer = function () {
 
             return parser;
         },
-        getTrackHandler:function() {
+        getTextTrackExtensions:function() {
             return this.system.getObject("textTrackExtensions");
         },
 
@@ -67,7 +65,6 @@ MediaPlayer.dependencies.TextVTTSourceBuffer = function () {
         }
     };
 };
-
 
 MediaPlayer.dependencies.TextVTTSourceBuffer.prototype = {
     constructor: MediaPlayer.dependencies.TextVTTSourceBuffer
