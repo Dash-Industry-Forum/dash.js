d55176309fff297c2a5938ad96af77597575de92
kozhinm <mikhail.kozhin@akvelon.com>
2014-11-21 13:24:29 +0300

    removes redundant ManifestUpdater.update call from ManifestUpdater.setup method

--- app/js/streaming/ManifestUpdater.js
+++ app/js/streaming/ManifestUpdater.js
@@ -101,7 +101,6 @@ MediaPlayer.dependencies.ManifestUpdater = function () {
         manifestLoader: undefined,
 
         setup: function () {
-            update.call(this);
             // Listen to streamsComposed event to be aware that the streams have been composed
             this.streamsComposed = onStreamsComposed;
             this.manifestLoaded = onManifestLoaded;
