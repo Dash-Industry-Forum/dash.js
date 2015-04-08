bcfe65ce67dbbb8ca304c8f038fecfb45c3b60ae
Rashmi Mittal <rmittal@adobe.com>
2015-04-08 13:04:11 -0700

    Remove the extra playback listeners from StreamController.js

--- src/streaming/controllers/StreamController.js
+++ src/streaming/controllers/StreamController.js
@@ -88,12 +88,6 @@
             playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_SEEKING, this);
             playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_TIME_UPDATED, this);
             playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_PROGRESS, this);
-
-            //Add playback play/pause/stop/seeked notifications
-            playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STARTED, this);
-            playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_PAUSED, this);
-            playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STOPPED, this);
-            playbackCtrl.subscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_SEEKED, this);
         },
 
         detachVideoEvents = function (stream) {
@@ -107,12 +101,6 @@
                 playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_SEEKING, self);
                 playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_TIME_UPDATED, self);
                 playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_PROGRESS, self);
-
-                //Add playback play/pause/stop/seeked notifications
-                playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STARTED, self);
-                playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_PAUSED, self);
-                playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STOPPED, self);
-                playbackCtrl.unsubscribe(MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_SEEKED, self);
             },1);
         },
 
@@ -353,19 +341,6 @@
             } else {
                 this.reset();
             }
-        },
-        
-        //Add playback play/pause/stop/seeked notifications
-        onSeeked = function () {
-        },
-
-        onStarted = function() {
-        },
-
-        onPaused = function() {
-        },
-
-        onStopped = function() {
         };
 
     return {
@@ -399,12 +374,6 @@
             this[MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_TIME_UPDATED] = onTimeupdate;
 
             this[MediaPlayer.dependencies.TimeSyncController.eventList.ENAME_TIME_SYNCHRONIZATION_COMPLETED] = onTimeSyncAttemptCompleted;
-
-            //Add playback play/pause/stop/seeked notifications
-            this[MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_SEEKED] = onSeeked;
-            this[MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STARTED] = onStarted;
-            this[MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_PAUSED] = onPaused;
-            this[MediaPlayer.dependencies.PlaybackController.eventList.ENAME_PLAYBACK_STOPPED] = onStopped;
         },
 
         setAutoPlay: function (value) {
