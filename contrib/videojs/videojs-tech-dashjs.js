e1211f2805fa0fbc95c3b0c7592724e0d78fac71
jrivera <jrivera@brightcove.com>
2015-05-11 10:52:20 -0400

    Started reworking the Dashjs tech to be more like the HLS tech

--- contrib/videojs/videojs-tech-dashjs.js
+++ contrib/videojs/videojs-tech-dashjs.js
@@ -2,7 +2,7 @@
  * videojs-tech-dash
  *
  * Use Dash.js to playback DASH content inside of Video.js
- * 
+ *
  * @param {vjs.Player|Object} player
  * @param {Object=} options
  * @param {Function=} ready
@@ -10,39 +10,65 @@
  */
 videojs.Dashjs = videojs.Html5.extend({
   init: function(player, options, ready){
-    var source, context, mediaPlayer;
+    var source;
 
     source = options.source;
+
     // need to remove the source so the HTML5 controller
     // doesn't try to use it
     delete options.source;
-
-    // Accept externally created context and media player references
-    // or default to baseline
-    context = player.options().context || new Dash.di.DashContext();
-    mediaPlayer = player.options().mediaPlayer || new MediaPlayer(context);
-
     // run the init of the HTML5 controller
     videojs.Html5.call(this, player, options, ready);
+    options.source = source;
 
-    // Must run controller before these two lines or else there is no
-    // element to bind to.
-    mediaPlayer.startup();
-    mediaPlayer.attachView(this.el());
-
-    // Dash.js autoplays by default
-    if (!options.autoplay) {
-      mediaPlayer.setAutoPlay(false);
-    }
-
-    // Attach the source
-    mediaPlayer.attachSource(source.src);
-
-    // Per DAN - people will need access to the context and mediaPlayer
-    player.mediaPlayer = mediaPlayer;
-    player.context = context;
+    videojs.Dashjs.prototype.src.call(this, options.source && options.source.src);
   }
 });
+
+videojs.Dashjs.prototype.src = function(src) {
+  var player = this.player();
+  var options = this.options();
+
+  // do nothing if the src is falsey
+  if (!src) {
+    return;
+  }
+  // Accept externally created context and media player references
+  // or default to baseline
+  var context = player.context || options.context || new Dash.di.DashContext();
+  var mediaPlayer = player.mediaPlayer || options.mediaPlayer || new MediaPlayer(context);
+
+  // if there is already a source loaded, clean it up
+  if (this.src_) {
+    this.resetSrc_();
+    if (player.mediaPlayer) {
+      player.mediaPlayer.reset();
+    }
+  }
+
+  // Must run controller before these two lines or else there is no
+  // element to bind to.
+  mediaPlayer.startup();
+  mediaPlayer.attachView(this.el());
+
+  // Dash.js autoplays by default
+  if (!options.autoplay) {
+    mediaPlayer.setAutoPlay(false);
+  }
+
+  var protData = options.protData || {
+      "com.widevine.alpha": {
+          "laURL": "http://wvlic.qa.brightcove.com/proxy/30328521339202"
+      }
+  };
+
+  // Attach the source
+  mediaPlayer.attachSource(src, null, protData);
+
+  // Per DAN - people will need access to the context and mediaPlayer
+  player.mediaPlayer = mediaPlayer;
+  player.context = context;
+};
 
 videojs.Dashjs.prototype.currentTime = function() {
   if(this.player().mediaPlayer) {
@@ -79,12 +105,8 @@ videojs.Dashjs.isSupported = function(){
 };
 
 videojs.Dashjs.canPlaySource = function(srcObj){
-  if (srcObj.type === 'application/dash+xml') {
-    // TODO: allow codec info and check browser support
-    return 'maybe';
-  } else {
-    return '';
-  }
+  var dashRE = /^application\/dash\+xml/i;
+  return dashRE.test(srcObj.type);
 };
 
 // add this to the top of the list of available media playback technologies
