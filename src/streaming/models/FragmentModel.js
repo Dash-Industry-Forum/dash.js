b6997dbb7d81d55a6e63fc12eb9d4812aabb1ae0
Karishma Bagga <kbagga@adobe.com>
2015-05-27 16:03:59 +0530

    Bug fix in abortRequests

--- src/streaming/models/FragmentModel.js
+++ src/streaming/models/FragmentModel.js
@@ -352,13 +352,17 @@ MediaPlayer.dependencies.FragmentModel = function () {
         },
 
         abortRequests: function() {
+            var reqs = [];
             this.fragmentLoader.abort();
 
-            for (var i = 0, ln = loadingRequests.length; i < ln; i += 1) {
-                removeRequest.call(this, executedRequests, loadingRequests[i]);
+            while (loadingRequests.length > 0) {
+                reqs.push(loadingRequests[0]);
+                removeRequest.call(this, loadingRequests, loadingRequests[0]);
             }
 
             loadingRequests = [];
+            
+            return reqs;
         },
 
         executeRequest: function(request) {
