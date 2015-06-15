cc335907b13ce17aec8ce4cc380fac65a7f25b32
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2015-06-15 13:32:33 +0100

    Remove old code, handle previousPlaybackRate not being defined

--- src/streaming/models/VideoModel.js
+++ src/streaming/models/VideoModel.js
@@ -75,7 +75,7 @@ MediaPlayer.models.VideoModel = function () {
 
             // If nothing is stalled resume playback.
             if (isStalled() === false) {
-                this.setPlaybackRate(previousPlaybackRate);
+                this.setPlaybackRate(previousPlaybackRate || 1);
                 element.dispatchEvent(new CustomEvent("playing"));
             }
         },
@@ -91,13 +91,7 @@ MediaPlayer.models.VideoModel = function () {
         onBufferLevelStateChanged = function(e) {
             var type = e.sender.streamProcessor.getType();
             stallStream.call(this, type, !e.data.hasSufficientBuffer);
-        }
-        /*,
-        handleSetCurrentTimeNotification = function () {
-            if (element.currentTime !== _currentTime) {
-                element.currentTime = _currentTime;
-            }
-        }*/;
+        };
 
     return {
         system : undefined,
