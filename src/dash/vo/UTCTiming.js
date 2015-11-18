d08d84ec5de1151cccd68fe338a6eeffee1ff588
Dmitriy Chernenko <dmitriy.chernenko@gmail.com>
2015-11-18 17:39:09 +0200

    Convert vo classes

--- src/dash/vo/UTCTiming.js
+++ src/dash/vo/UTCTiming.js
@@ -28,18 +28,13 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-/*globals Dash */
 
-let UTCTiming = function () {
-    "use strict";
-
-    // UTCTiming is a DescriptorType and doesn't have any additional fields
-    this.schemeIdUri = "";
-    this.value = "";
-};
-
-UTCTiming.prototype = {
-    constructor: UTCTiming
-};
+class UTCTiming {
+    constructor (){
+        // UTCTiming is a DescriptorType and doesn't have any additional fields
+        this.schemeIdUri = "";
+        this.value = "";
+    }
+}
 
 export default UTCTiming;
