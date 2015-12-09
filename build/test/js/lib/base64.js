c73d4c8ff431b6fc8603279a70c62c479e76eda6
Torbjorn Einarsson <teinarsson@mobitv.com>
2015-12-09 13:30:51 +0100

    Aligned content of directory build/test/js/lib with that of src/lib.

--- build/test/js/lib/base64.js
+++ build/test/js/lib/base64.js
@@ -144,9 +144,14 @@ var BASE64 = {};
     };
 })("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
 
+/*
+The following polyfills are not used in dash.js but have caused multiplayer integration issues.
+Therefore commenting them out.
+
 if (undefined === btoa) {
     var btoa = BASE64.encode;
 }
 if (undefined === atob) {
     var atob = BASE64.decode;
 }
+*/
