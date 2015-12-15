45d827d3dec2acdda64e99e2b92cd65067f70ca8
Axel Delmas <axel@streamroot.io>
2015-12-15 11:23:33 +0100

    Removed unused XlinkLoader import

--- src/streaming/controllers/XlinkController.js
+++ src/streaming/controllers/XlinkController.js
@@ -28,7 +28,6 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-import XlinkLoader from '../XLinkLoader.js';
 import EventBus from '../utils/EventBus.js';
 import Events from '../Events.js';
 import FactoryMaker from '../../core/FactoryMaker.js';
