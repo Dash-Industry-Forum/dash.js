443804bfce1e94b06d4047d7e7ed78ef9493e994
Dmitriy Chernenko <dmitriy.chernenko@gmail.com>
2015-12-23 18:45:51 +0200

    Converted FragmentController test

--- test/streaming.FragmentControllerSpec.js
+++ test/streaming.FragmentControllerSpec.js
@@ -1,58 +1,71 @@
+import VoHelper from './helpers/VOHelper.js';
+import Events from '../src/core/events/Events.js';
+import MediaPlayerEvents from '../src/streaming/MediaPlayerEvents.js';
+import FragmentController from '../src/streaming/controllers/FragmentController.js';
+
+const expect = require('chai').expect;
+
 describe("FragmentController", function () {
-    /*var voHelper = window.Helpers.getVOHelper(),
-        objectsHelper = window.Helpers.getObjectsHelper(),
-        fragmentController = objectsHelper.getFragmentController();
+    const context = {};
+    const voHelper = new VoHelper();
+    const fragmentController = FragmentController(context).create();
+    
+    Events.extend(MediaPlayerEvents);
 
     it("should process bytes array", function () {
-        var bytes = new ArrayBuffer(612),
-            expectedValue = new Uint8Array(bytes),
-            result;
+        const bytes = new ArrayBuffer(612);
+        const expectedValue = new Uint8Array(bytes);
 
-        result =  fragmentController.process(bytes);
-        expect(result).toEqual(expectedValue);
+        let result = fragmentController.process(bytes);
+
+        expect(result).to.be.eql(expectedValue);
     });
 
     it("should not create or return model without context", function () {
-        var expectedValue = null;
+        const expectedValue = null;
 
-        expect(fragmentController.getModel()).toEqual(expectedValue);
-        expect(fragmentController.getModel(null)).toEqual(expectedValue);
+        expect(fragmentController.getModel()).to.be.equal(expectedValue);
+        expect(fragmentController.getModel(null)).to.be.equal(expectedValue);
     });
 
     it("should create or return model for a given context", function () {
-        var context = {},
-            model = fragmentController.getModel(context);
+        const context = {};
 
-        expect(model).toBeDefined();
-        expect(model).not.toBeNull();
+        const model = fragmentController.getModel(context);
+
+        expect(model).to.exist;
     });
 
     it("should always return the same model for the context", function () {
-        var context1 = 1,
-            context2 = 2,
-            model1 = fragmentController.getModel(context1),
-            model2 = fragmentController.getModel(context2);
+        const context1 = 1;
+        const context2 = 2;
 
-        expect(fragmentController.getModel(context1)).toEqual(model1);
-        expect(fragmentController.getModel(context2)).toEqual(model2);
+        const model1 = fragmentController.getModel(context1);
+        const model2 = fragmentController.getModel(context2);
+
+        expect(fragmentController.getModel(context1)).to.be.equal(model1);
+        expect(fragmentController.getModel(context2)).to.be.equal(model2);
     });
 
     it("should detach model", function () {
-        var context = {},
-            model = fragmentController.getModel(context);
-
+        const context = {};
+        const model = fragmentController.getModel(context);
+        
         fragmentController.detachModel(model);
-        expect(fragmentController.getModel(context)).not.toEqual(model);
+        
+        expect(fragmentController.getModel(context)).to.be.not.equal(model);
     });
 
     it("should identify an initialization segment", function () {
         var request = voHelper.getInitRequest();
+        expect(fragmentController.isInitializationRequest(request)).to.be.ok;
 
-        expect(fragmentController.isInitializationRequest(null)).toBeFalsy();
-        expect(fragmentController.isInitializationRequest(request)).toBeTruthy();
         request.type = "unknown";
-        expect(fragmentController.isInitializationRequest(null)).toBeFalsy();
+        expect(fragmentController.isInitializationRequest(request)).to.not.be.ok;
+
         request.type = undefined;
-        expect(fragmentController.isInitializationRequest(null)).toBeFalsy();
-    });*/
+        expect(fragmentController.isInitializationRequest(request)).to.not.be.ok;
+
+        expect(fragmentController.isInitializationRequest(null)).to.not.be.ok;
+    });
 });
