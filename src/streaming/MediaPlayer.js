582f9d1ab8daf9822c721b330d852897510d4248
David Evans <david.evans@rd.bbc.co.uk>
2016-01-27 15:53:21 +0000

    use correct key when saving settings to local storage

--- src/streaming/MediaPlayer.js
+++ src/streaming/MediaPlayer.js
@@ -1487,7 +1487,6 @@ function MediaPlayer() {
 
         mediaController.initialize();
         mediaController.setConfig({
-            DOMStorage: domStorage,
             errHandler: errHandler
         });
 
