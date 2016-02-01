8e61fb639cb070be88b48781bc77a09c83123c11
David Evans <david.evans@rd.bbc.co.uk>
2016-02-01 08:38:07 +0000

    Re-add STREAM_TEARDOWN_COMPLETE event needed for metrics reset

--- src/streaming/controllers/StreamController.js
+++ src/streaming/controllers/StreamController.js
@@ -720,6 +720,8 @@ function StreamController() {
                 eventBus.trigger(Events.PROTECTION_DESTROYED, {data: manifestModel.getValue().url});
             }
         }
+
+        eventBus.trigger(Events.STREAM_TEARDOWN_COMPLETE);
     }
 
     instance = {
