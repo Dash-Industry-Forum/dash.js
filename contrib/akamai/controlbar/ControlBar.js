c4868837e9336ef8280cbc3d94644d9881101557
Will Law <wilaw@akamai.com>
2016-04-07 17:30:13 -0700

    fixing commit of incorrect controlbar files

--- contrib/akamai/controlbar/ControlBar.js
+++ contrib/akamai/controlbar/ControlBar.js
@@ -28,6 +28,7 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
+
 var ControlBar = function(dashjsMediaPlayer) {
 
     var player = dashjsMediaPlayer,
