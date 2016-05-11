ab0e6a307f0fdbe2b5f858d72add6ee4b70ebe96
James Reed <jamesreedcode@users.noreply.github.com>
2016-05-11 14:19:56 +0100

    always update segments when dynamic and make sure we filter out any not available on the server

--- src/dash/utils/TimelineConverter.js
+++ src/dash/utils/TimelineConverter.js
@@ -143,11 +143,11 @@ function TimelineConverter() {
         var range = { start: start, end: end };
         var hasSegments = representation.segments && representation.segments.length;
         var d = representation.segmentDuration || (hasSegments ? representation.segments[representation.segments.length - 1].duration : 0);
-        var availableSegmentsStartTime = hasSegments && representation.segments[0].availabilityStartTime.getTime() / 1000;
-        var availableSegmentsEndTime = hasSegments && representation.segments[representation.segments.length - 1].availabilityStartTime.getTime() / 1000;
 
         var checkTime,
-            now;
+            now,
+            availableSegmentsStartTime,
+            availableSegmentsEndTime;
 
         if (!isDynamic) return range;
 
@@ -157,6 +157,7 @@ function TimelineConverter() {
 
         checkTime = representation.adaptation.period.mpd.checkTime;
         now = calcPresentationTimeFromWallTime(new Date(), representation.adaptation.period);
+
         //the Media Segment list is further restricted by the CheckTime together with the MPD attribute
         // MPD@timeShiftBufferDepth such that only Media Segments for which the sum of the start time of the
         // Media Segment and the Period start time falls in the interval [NOW- MPD@timeShiftBufferDepth - @duration, min(CheckTime, NOW)] are included.
@@ -164,11 +165,35 @@ function TimelineConverter() {
         var timeAnchor = (isNaN(checkTime) ? now : Math.min(checkTime, now));
         var periodEnd = representation.adaptation.period.start + representation.adaptation.period.duration;
         end = (timeAnchor >= periodEnd  && (timeAnchor - d) < periodEnd ? periodEnd : timeAnchor) - d;
+
+        // We have approximate numbers now, but we need to adjust this based on when segments actually
+        // appear and disappear from the server - restrict the range to the first one which is still available,
+        // and the last one which is available.
+        if (hasSegments) {
+            for (let i = 0; i < representation.segments.length - 1; i++) {
+                let segmentAvailabilityEnd = representation.segments[i].availabilityEndTime.getTime() / 1000;
+                let segmentMediaStart = representation.segments[i].presentationStartTime;
+                if (segmentAvailabilityEnd >= start) {
+                    availableSegmentsStartTime = segmentMediaStart;
+                    break;
+                }
+            }
+
+            for (let i = representation.segments.length - 1; i > 0; i--) {
+                let segmentAvailabilityStart = representation.segments[i].availabilityStartTime.getTime() / 1000;
+                let segmentMediaStart = representation.segments[i].presentationStartTime;
+                if (segmentAvailabilityStart <= now) {
+                    availableSegmentsEndTime = segmentMediaStart + representation.segments[i].duration;
+                    break;
+                }
+            }
+        }
+
         if (!disableSnapToSegmentBoundary) {
             if (availableSegmentsEndTime && end > availableSegmentsEndTime) {
                 end = availableSegmentsEndTime;
             }
-            if (availableSegmentsEndTime && start < availableSegmentsStartTime) {
+            if (availableSegmentsStartTime && start < availableSegmentsStartTime) {
                 start = availableSegmentsStartTime;
             }
         }
