7e127bd3475596350bfd002942e8ab0b62302147
Dan Sparacio <dsparaci@akamai.com>
2016-09-15 10:19:29 -0700

    adding back in hooks to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -231,6 +231,11 @@ module.exports = function (grunt) {
             options: {
                 config: '.jscsrc'
             }
+        },
+        githooks: {
+            all: {
+                'pre-commit': 'lint'
+            }
         }
     });
 
@@ -243,5 +248,5 @@ module.exports = function (grunt) {
     grunt.registerTask('release',   ['default', 'jsdoc']);
     grunt.registerTask('debug',     ['clean', 'browserify:all', 'exorcise:all', 'copy:dist']);
     grunt.registerTask('lint',      ['jshint', 'jscs']);
-    grunt.registerTask('prepublish', ['dist']);
+    grunt.registerTask('prepublish', ['githooks', 'dist']);
 };
