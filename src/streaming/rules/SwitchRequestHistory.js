5653d94f67698eacc3282ea351d9019de8536089
Jonty Usborne <jonty.usborne@bbc.co.uk>
2016-11-23 16:30:24 +0000

    Limit depth of switchRequests to recent history

--- src/streaming/rules/SwitchRequestHistory.js
+++ src/streaming/rules/SwitchRequestHistory.js
@@ -1,23 +1,41 @@
 
 import FactoryMaker from '../../core/FactoryMaker.js';
 
+const SWITCH_REQUEST_HISTORY_DEPTH = 6;
 
 function SwitchRequestHistory() {
-    let switchRequests = [];
+    let switchRequests = []; // running total
+    let srHistory = []; // history of each switch
 
     function push(switchRequest) {
+
         if (!switchRequests[switchRequest.oldValue]) {
             switchRequests[switchRequest.oldValue] = {noDrops: 0, drops: 0, dropSize: 0};
         }
 
+        // Set switch details
         let indexDiff = switchRequest.newValue - switchRequest.oldValue;
+        let drop = (indexDiff < 0) ? 1 : 0;
+        let dropSize = drop ? -indexDiff : 0;
+        let noDrop = drop ? 0 : 1;
 
-        if (indexDiff < 0) {
-            switchRequests[switchRequest.oldValue].drops++;
-            switchRequests[switchRequest.oldValue].dropSize += -indexDiff;
-        } else {
-            switchRequests[switchRequest.oldValue].noDrops++;
+        // Update running totals
+        switchRequests[switchRequest.oldValue].drops += drop;
+        switchRequests[switchRequest.oldValue].dropSize += dropSize;
+        switchRequests[switchRequest.oldValue].noDrops += noDrop;
+
+        // Save to history
+        srHistory = srHistory || [];
+        srHistory.push({idx: switchRequest.oldValue, noDrop: noDrop, drop: drop, dropSize: dropSize});
+
+        // Shift earliest switch off srHistory and readjust to keep depth of running totals constant
+        if ( srHistory.length > SWITCH_REQUEST_HISTORY_DEPTH ) {
+            let srHistoryFirst = srHistory.shift();
+            switchRequests[srHistoryFirst.idx].drops -= srHistoryFirst.drop;
+            switchRequests[srHistoryFirst.idx].dropSize -= srHistoryFirst.dropSize;
+            switchRequests[srHistoryFirst.idx].noDrops -= srHistoryFirst.noDrop;
         }
+
     }
 
     function getSwitchRequests() {
