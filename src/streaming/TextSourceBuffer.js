528fe5f2d6ab6355d22710d71bc3ca0d4e03a7d8
Nicolas ANGOT <nicolas.angot@orange.com>
2016-12-02 15:53:04 +0100

    first version of imscJS library integration.

--- src/streaming/TextSourceBuffer.js
+++ src/streaming/TextSourceBuffer.js
@@ -848,7 +848,6 @@ function TextSourceBuffer() {
             parser = VTTParser;
         } else if (codecType.search('ttml') >= 0 || codecType.search('stpp') >= 0) {
             parser = TTMLParser;
-            parser.setConfig({videoModel: videoModel});
         }
         return parser;
     }
