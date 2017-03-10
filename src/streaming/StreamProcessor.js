a96f3def3e535279d7c2f8e08be714a41db9d9ad
Nicolas ANGOT <nicolas.angot@orange.com>
2017-03-10 15:50:59 +0100

    rulesController reference is not used in ScheduleController : remove it.

--- src/streaming/StreamProcessor.js
+++ src/streaming/StreamProcessor.js
@@ -35,7 +35,6 @@ import StreamController from './controllers/StreamController';
 import MediaController from './controllers/MediaController';
 import TextController from './controllers/TextController';
 import ScheduleController from './controllers/ScheduleController';
-import RulesController from './rules/RulesController';
 import MediaPlayerModel from './models/MediaPlayerModel';
 import MetricsModel from './models/MetricsModel';
 import FragmentLoader from './FragmentLoader';
@@ -98,7 +97,6 @@ function StreamProcessor(config) {
             dashMetrics: DashMetrics(context).getInstance(),
             dashManifestModel: DashManifestModel(context).getInstance(),
             timelineConverter: timelineConverter,
-            rulesController: RulesController(context).getInstance(),
             mediaPlayerModel: MediaPlayerModel(context).getInstance(),
         });
 
@@ -117,8 +115,6 @@ function StreamProcessor(config) {
 
         representationController = RepresentationController(context).create();
         representationController.initialize(this);
-
-
     }
 
     function reset(errored) {
