Dash-Industry-Forum/dash.js

commit ed2005a31df7e227a295de8281a565c5604d41df

Subtitle Refactoring :
- Use proxy TextBufferController to instanciate fragmentedText buffer controller

src/streaming/text/TextBufferController.js
@@ -29,7 +29,8 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 import FactoryMaker from '../../core/FactoryMaker';
-import NotFragmentedTextController from './NotFragmentedTextController';
+import BufferController from './../controllers/BufferController';
+import NotFragmentedTextSourceBufferController from './NotFragmentedTextSourceBufferController';
 
 function TextBufferController(config) {
 
@@ -42,10 +43,27 @@ function TextBufferController(config) {
     function setup(config) {
 
         // according to text type, we create corresponding buffer controller
-        _BufferControllerImpl = NotFragmentedTextController(context).create({
-            errHandler: config.errHandler,
-            sourceBufferController: config.sourceBufferController
-        });
+        if (config.type === 'fragmentedText') {
+
+            // in this case, internal buffer ocntroller is a classical BufferController object
+            _BufferControllerImpl = BufferController(context).create({
+                metricsModel: config.metricsModel,
+                manifestModel: config.manifestModel,
+                sourceBufferController: config.sourceBufferController,
+                errHandler: config.errHandler,
+                streamController: config.streamController,
+                mediaController: config.mediaController,
+                adapter: config.adapter,
+                textController: config.textController
+            });
+        } else {
+
+            // in this case, internal buffer controller is a not fragmented text controller  object
+            _BufferControllerImpl = NotFragmentedTextSourceBufferController(context).create({
+                errHandler: config.errHandler,
+                sourceBufferController: config.sourceBufferController
+            });
+        }
     }
 
     function initialize(Type, source, StreamProcessor) {
@@ -61,6 +79,10 @@ function TextBufferController(config) {
         return _BufferControllerImpl.createBuffer(mediaInfo);
     }
 
+    function getType() {
+        return _BufferControllerImpl.getType();
+    }
+
     function getBuffer() {
         return _BufferControllerImpl.getBuffer();
     }
@@ -69,12 +91,28 @@ function TextBufferController(config) {
         _BufferControllerImpl.setBuffer(value);
     }
 
+    function getMediaSource() {
+        return _BufferControllerImpl.getMediaSource();
+    }
+
     function setMediaSource(value) {
         _BufferControllerImpl.setMediaSource(value);
     }
 
+    function setStreamProcessor(streamProcessor) {
+        _BufferControllerImpl.setStreamProcessor(streamProcessor);
+    }
+
     function getStreamProcessor() {
         _BufferControllerImpl.getStreamProcessor();
+    }
+
+    function getBufferLevel() {
+        return _BufferControllerImpl.getBufferLevel();
+    }
+
+    function getCriticalBufferLevel() {
+        return _BufferControllerImpl.getCriticalBufferLevel();
     }
 
     function reset(errored) {
@@ -92,11 +130,16 @@ function TextBufferController(config) {
     instance = {
         initialize: initialize,
         createBuffer: createBuffer,
+        getType: getType,
+        getStreamProcessor: getStreamProcessor,
+        setStreamProcessor: setStreamProcessor,
         getBuffer: getBuffer,
         setBuffer: setBuffer,
-        getStreamProcessor: getStreamProcessor,
-        getIsBufferingCompleted: getIsBufferingCompleted,
+        getBufferLevel: getBufferLevel,
+        getCriticalBufferLevel: getCriticalBufferLevel,
         setMediaSource: setMediaSource,
+        getMediaSource: getMediaSource,
+        getIsBufferingCompleted: getIsBufferingCompleted,
         switchInitData: switchInitData,
         reset: reset
     };
@@ -107,4 +150,5 @@ function TextBufferController(config) {
 }
 
 TextBufferController.__dashjs_factory_name = 'TextBufferController';
+export default FactoryMaker.getClassFactory(TextBufferController);
 export default FactoryMaker.getClassFactory(TextBufferController);

src/streaming/StreamProcessor.js
@@ -255,7 +255,7 @@ function StreamProcessor(config) {
     function createBufferControllerForType(type) {
         var controller = null;
 
-        if (type === 'video' || type === 'audio' || type === 'fragmentedText') {
+        if (type === 'video' || type === 'audio') {
             controller = BufferController(context).create({
                 metricsModel: MetricsModel(context).getInstance(),
                 manifestModel: manifestModel,
@@ -268,8 +268,15 @@ function StreamProcessor(config) {
             });
         }else {
             controller = TextBufferController(context).create({
+                type: type,
+                metricsModel: MetricsModel(context).getInstance(),
+                manifestModel: manifestModel,
+                sourceBufferController: SourceBufferController(context).getInstance(),
                 errHandler: ErrorHandler(context).getInstance(),
-                sourceBufferController: SourceBufferController(context).getInstance()
+                streamController: StreamController(context).getInstance(),
+                mediaController: MediaController(context).getInstance(),
+                adapter: adapter,
+                textController: TextController(context).getInstance()
             });
         }
 

src/streaming/text/NotFragmentedTextSourceBufferController.js
@@ -33,7 +33,7 @@ import Events from '../../core/events/Events';
 import FactoryMaker from '../../core/FactoryMaker';
 import InitCache from '../utils/InitCache';
 
-function NotFragmentedTextController(config) {
+function NotFragmentedTextSourceBufferController(config) {
 
     let context = this.context;
     let eventBus = EventBus(context).getInstance();
@@ -95,6 +95,10 @@ function NotFragmentedTextController(config) {
         return buffer;
     }
 
+    function getType() {
+        return type;
+    }
+
     function getBuffer() {
         return buffer;
     }
@@ -107,8 +111,24 @@ function NotFragmentedTextController(config) {
         mediaSource = value;
     }
 
+    function getMediaSource() {
+        return mediaSource;
+    }
+
+    function setStreamProcessor(value) {
+        streamProcessor = value;
+    }
+
     function getStreamProcessor() {
         return streamProcessor;
+    }
+
+    function getBufferLevel() {
+        return 0;
+    }
+
+    function getCriticalBufferLevel() {
+        return 0;
     }
 
     function reset(errored) {
@@ -124,7 +144,10 @@ function NotFragmentedTextController(config) {
 
     function onDataUpdateCompleted(e) {
         if (e.sender.getStreamProcessor() !== streamProcessor) return;
-        eventBus.trigger(Events.TIMED_TEXT_REQUESTED, { index: 0, sender: e.sender }); //TODO make index dynamic if referring to MP?
+        eventBus.trigger(Events.TIMED_TEXT_REQUESTED, {
+            index: 0,
+            sender: e.sender
+        }); //TODO make index dynamic if referring to MP?
     }
 
     function onInitFragmentLoaded(e) {
@@ -141,18 +164,25 @@ function NotFragmentedTextController(config) {
         if (chunk) {
             sourceBufferController.append(chunk);
         } else {
-            eventBus.trigger(Events.INIT_REQUESTED, {sender: instance});
+            eventBus.trigger(Events.INIT_REQUESTED, {
+                sender: instance
+            });
         }
     }
 
     instance = {
         initialize: initialize,
         createBuffer: createBuffer,
+        getType: getType,
+        getStreamProcessor: getStreamProcessor,
+        setStreamProcessor: setStreamProcessor,
         getBuffer: getBuffer,
         setBuffer: setBuffer,
-        getStreamProcessor: getStreamProcessor,
-        getIsBufferingCompleted: getIsBufferingCompleted,
+        getBufferLevel: getBufferLevel,
+        getCriticalBufferLevel: getCriticalBufferLevel,
         setMediaSource: setMediaSource,
+        getMediaSource: getMediaSource,
+        getIsBufferingCompleted: getIsBufferingCompleted,
         switchInitData: switchInitData,
         reset: reset
     };
@@ -162,5 +192,5 @@ function NotFragmentedTextController(config) {
     return instance;
 }
 
-NotFragmentedTextController.__dashjs_factory_name = 'NotFragmentedTextController';
-export default FactoryMaker.getClassFactory(NotFragmentedTextController);
+NotFragmentedTextSourceBufferController.__dashjs_factory_name = 'NotFragmentedTextSourceBufferController';
+export default FactoryMaker.getClassFactory(NotFragmentedTextSourceBufferController);

commit 1d4d2b5e8449f98119e45d1e95dfb553deec22c7

fix(TextBufferContoller): only export one default
Caused by ed2005a31df7e227a295de8281a565c5604d41df

src/streaming/text/TextBufferController.js
@@ -151,4 +151,3 @@ function TextBufferController(config) {
 
 TextBufferController.__dashjs_factory_name = 'TextBufferController';
 export default FactoryMaker.getClassFactory(TextBufferController);
-export default FactoryMaker.getClassFactory(TextBufferController);