ce9a051785d471b916fd254bb2a2cf0a739b89a1
Nicolas ANGOT <nicolas.angot@orange.com>
2017-04-28 15:53:20 +0200

    fix wrong path...

--- src/streaming/text/TextTracks.js
+++ src/streaming/text/TextTracks.js
@@ -28,10 +28,10 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-import EventBus from '../core/EventBus';
-import Events from '../core/events/Events';
-import FactoryMaker from '../core/FactoryMaker';
-import Debug from '../core/Debug';
+import EventBus from '../../core/EventBus';
+import Events from '../../core/events/Events';
+import FactoryMaker from '../../core/FactoryMaker';
+import Debug from '../../core/Debug';
 import { renderHTML } from 'imsc';
 
 function TextTracks() {
