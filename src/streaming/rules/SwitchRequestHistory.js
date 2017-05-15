c7451d53a86023893013c592f1c3042dba92ce0d
Thomas De Keulenaer <thomas.dekeulenaer@gmail.com>
2017-05-15 13:46:42 +0200

    Fixed import

--- src/streaming/rules/SwitchRequestHistory.js
+++ src/streaming/rules/SwitchRequestHistory.js
@@ -1,6 +1,6 @@
 
 import FactoryMaker from '../../core/FactoryMaker.js';
-import SwitchRequest from 'SwitchRequest.js';
+import SwitchRequest from './SwitchRequest.js';
 
 const SWITCH_REQUEST_HISTORY_DEPTH = 8; // must be > SwitchHistoryRule SAMPLE_SIZE to enable rule
 
