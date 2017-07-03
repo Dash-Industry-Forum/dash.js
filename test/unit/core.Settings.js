36e093fd28b2ee2bb41541a99a989cea6c3693e5
Robert Bryer <robert.bryer@bbc.co.uk>
2017-07-03 11:19:03 +0100

    Add a test for not applying wrong settings

--- test/unit/core.Settings.js
+++ test/unit/core.Settings.js
@@ -1,5 +1,6 @@
 import Settings from '../../src/core/Settings';
 import Utils from '../../src/core/Utils';
+import Debug from '../../src/core/Debug';
 
 const chai = require('chai');
 const expect = chai.expect;
@@ -8,9 +9,18 @@ const context = {};
 
 describe('Settings', function(){
 
+    before(() => {
+        const debug = Debug(context).getInstance();
+        expect(debug).to.exist;
+        debug.setLogToBrowserConsole(false);
+    });
+
+    beforeEach(() => {
+        Settings(context).getInstance().reset();
+    });
+
     it('should have a default settings object at first load', () => {
         const settings = Settings(context).getInstance();
-
         const def = settings.get();
 
         expect(def.streaming.abandonLoadTimeout).to.equal(10000);
@@ -35,16 +45,35 @@ describe('Settings', function(){
         settings.update(s);
 
         const res = settings.get();
-
         expect(res.streaming.abandonLoadTimeout).to.equal(20000);
         expect(res.streaming.liveDelay).to.equal(30);
         expect(res.streaming.abr.limitBitrateByPortal).to.be.true;
         expect(res.streaming.abr.maxBitrate.audio).to.equal(300);
         expect(res.streaming.abr.maxBitrate.video).to.equal(2000);
-
     });
 
-    /*it('should not apply settings that fall outside default object', () => {
+    it('should not apply elements that are not part of the settings object', () => {
         const settings = Settings(context).getInstance();
-    });*/
+        const s = {
+            streaming: {
+                notASetting: 20000,
+                chickens: 30,
+                abr: {
+                    geese: true,
+                    maxBitrate: { ducks: 300, video: 2000 }
+                }
+            },
+            resplendantQuetzal: false
+        };
+
+        settings.update(s);
+
+        const res = settings.get();
+        expect(res.streaming.notASetting).to.be.undefined;
+        expect(res.streaming.chickens).to.be.undefined;
+        expect(res.streaming.abr.geese).to.be.undefined;
+        expect(res.streaming.abr.maxBitrate.ducks).to.be.undefined;
+        expect(res.resplendantQuetzal).to.be.undefined;
+        expect(res.streaming.abr.maxBitrate.video).to.be.equal(2000);
+    });
 });
