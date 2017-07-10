64966c86604d79f48467083559ac9fa65acfaf55
Nicolas ANGOT <nicolas.angot@orange.com>
2017-07-10 12:59:01 +0200

    update CustomTimeRanges unit test

--- test/unit/streaming.utils.CustomTimeRanges.js
+++ test/unit/streaming.utils.CustomTimeRanges.js
@@ -12,4 +12,24 @@ describe('CustomTimeRanges', function () {
         expect(customTimeRangeArray).to.be.instanceOf(Array);    // jshint ignore:line
         expect(customTimeRangeArray).to.be.empty;                // jshint ignore:line
     });
+
+    it("should throw an exception when attempting to call start with a parameter which is not an integer", function () {
+        expect(customTimeRanges.start.bind(customTimeRanges,'t')).to.throw('index argument is not an integer');
+    });
+
+    it("should throw an exception when attempting to call end with a parameter which is not an integer", function () {
+        expect(customTimeRanges.end.bind(customTimeRanges,'t')).to.throw('index argument is not an integer');
+    });
+
+    it("should return NaN when start function is called with an index bigger than customTimeRangeArray.length", function () {
+        const start = customTimeRanges.start(2);
+
+        expect(start).to.be.NaN;                // jshint ignore:line 
+    });
+
+    it("should return NaN when end function is called with an index bigger than customTimeRangeArray.length", function () {
+        const end = customTimeRanges.end(2);
+
+        expect(end).to.be.NaN;                // jshint ignore:line 
+    });
 }); 
