66ed993e07b33cbc468d3c721239005b326eb796
Kevin Spiteri <kspiteri@ieee.org>
2017-07-13 16:35:08 -0700

    remove unused variables

--- src/streaming/rules/abr/ThroughputRule.js
+++ src/streaming/rules/abr/ThroughputRule.js
@@ -40,13 +40,6 @@ function ThroughputRule(config) {
     const log = Debug(context).getInstance().log;
     const metricsModel = config.metricsModel;
 
-    let throughputArray,
-        latencyArray;
-
-    function setup() {
-        reset();
-    }
-
     function checkConfig() {
         if (!metricsModel || !metricsModel.hasOwnProperty('getReadOnlyMetricsFor')) {
             throw new Error('Missing config parameter(s)');
@@ -68,11 +61,11 @@ function ThroughputRule(config) {
         const metrics = metricsModel.getReadOnlyMetricsFor(mediaType);
         const streamProcessor = rulesContext.getStreamProcessor();
         const abrController = rulesContext.getAbrController();
-        const throughputHistory = abrController.getThroughputHistory();
         const streamInfo = rulesContext.getStreamInfo();
         const isDynamic = streamInfo && streamInfo.manifestInfo ? streamInfo.manifestInfo.isDynamic : null;
-        let throughput = throughputHistory.getSafeAverageThroughput(mediaType, isDynamic);
-        let latency = throughputHistory.getAverageLatency(mediaType);
+        const throughputHistory = abrController.getThroughputHistory();
+        const throughput = throughputHistory.getSafeAverageThroughput(mediaType, isDynamic);
+        const latency = throughputHistory.getAverageLatency(mediaType);
         const bufferStateVO = (metrics.BufferState.length > 0) ? metrics.BufferState[metrics.BufferState.length - 1] : null;
         const hasRichBuffer = rulesContext.hasRichBuffer();
 
@@ -94,8 +87,7 @@ function ThroughputRule(config) {
     }
 
     function reset() {
-        throughputArray = [];
-        latencyArray = [];
+        // no persistent information to reset
     }
 
     const instance = {
@@ -103,7 +95,6 @@ function ThroughputRule(config) {
         reset: reset
     };
 
-    setup();
     return instance;
 }
 
