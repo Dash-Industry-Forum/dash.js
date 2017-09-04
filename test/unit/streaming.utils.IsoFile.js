dc94901b87d9892daf963d6c42a681e9c040f972
Nicolas ANGOT <nicolas.angot@orange.com>
2017-09-04 18:36:24 +0200

    update IsoFile unit test for getLastBox and getBoxes function

--- test/unit/streaming.utils.IsoFile.js
+++ test/unit/streaming.utils.IsoFile.js
@@ -12,7 +12,8 @@ describe('IsoFile', function () {
 		it('should return null when getBoxes is called and no parsed file has been set', () => {
 				const boxes = isoFile.getBoxes('test');
 		        
-		        expect(boxes).to.be.null;  // jshint ignore:line
+		        expect(boxes).to.be.instanceOf(Array);    // jshint ignore:line
+	            expect(boxes).to.be.empty;                // jshint ignore:line
 		});
 
 		it('should return null when getBox is called and no parsed file has been set', () => {
@@ -27,11 +28,27 @@ describe('IsoFile', function () {
 	        expect(lastBox).to.be.null;  // jshint ignore:line
 		});
 	});
-	describe('when no type is defined', () => {
+	describe('when incorrect parsed file has been set', () => {
 		it('should return an empty array when getBoxes is called and type is undefined', () => {
 				const parsedFile = ISOBoxer.parseBuffer(new ArrayBuffer(10));
 				isoFile.setData(parsedFile);
 				const boxes = isoFile.getBoxes();
+		        
+		        expect(boxes).to.be.instanceOf(Array);    // jshint ignore:line
+	            expect(boxes).to.be.empty;                // jshint ignore:line
+		});
+
+		it('should return null when getLastBox is called', () => {
+				isoFile.setData({boxes: [{type: 'typeA'}, {type: 'typeB'}]});
+				const box = isoFile.getLastBox();
+		        
+	            expect(box).to.be.null;                // jshint ignore:line
+		});
+
+		it('should return an empty array when getBoxes is called', () => {
+				const parsedFile = ISOBoxer.parseBuffer(new ArrayBuffer(10));
+				isoFile.setData(parsedFile);
+				const boxes = isoFile.getBoxes('typeA');
 		        
 		        expect(boxes).to.be.instanceOf(Array);    // jshint ignore:line
 	            expect(boxes).to.be.empty;                // jshint ignore:line
