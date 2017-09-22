ccb54d4d284ebac2ab363908cab6ee26ea0c5d64
Nicolas ANGOT <nicolas.angot@orange.com>
2017-09-22 09:41:22 +0200

    use SwitchRequest.NO_CHANGE const in unit test.

--- test/unit/streaming.rules.abr.SwitchHistoryRule.js
+++ test/unit/streaming.rules.abr.SwitchHistoryRule.js
@@ -1,4 +1,5 @@
 import SwitchHistoryRule from '../../src/streaming/rules/abr/SwitchHistoryRule';
+import SwitchRequest from '../../src/streaming/rules/SwitchRequest';
 
 import RulesContextMock from './mocks/RulesContextMock';
 
@@ -10,7 +11,7 @@ describe('SwitchHistoryRule', function () {
     it("should return an empty switchRequest when getMaxIndex function is called with an empty parameter", function () {
         const switchRequest = switchHistoryRule.getMaxIndex();
 
-        expect(switchRequest.quality).to.be.equal(-1);  // jshint ignore:line 
+        expect(switchRequest.quality).to.be.equal(SwitchRequest.NO_CHANGE);  // jshint ignore:line 
     });
 
     it("should return an switchRequest with quality equals 0 when one switchRequest equals to {drops: 7, noDrops: 0, dropSize: 4}, a division by zero occurs", function () {
