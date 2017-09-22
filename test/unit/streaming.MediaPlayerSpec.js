da3df9f1ed0e2646536d21a36b1db19830ac71c2
Nicolas ANGOT <nicolas.angot@orange.com>
2017-09-22 15:25:05 +0200

    use defined string in MediaPlayer class.

--- test/unit/streaming.MediaPlayerSpec.js
+++ test/unit/streaming.MediaPlayerSpec.js
@@ -20,10 +20,6 @@ describe('MediaPlayer', function () {
         delete global.dashjs;
     });
 
-    const NOT_INITIALIZED_ERROR_MSG = 'MediaPlayer not initialized!';
-    const PLAYBACK_NOT_INITIALIZED_ERROR = 'You must first call initialize() to init playback before calling this method';
-    const ELEMENT_NOT_ATTACHED_ERROR = 'You must first call attachView() to set the video element before calling this method';
-
     const context = {};
     const specHelper = new SpecHelper();
     let dummyUrl = specHelper.getDummyUrl();
@@ -112,67 +108,67 @@ describe('MediaPlayer', function () {
     describe('Playback Functions', function () {
         describe('When it is not initialized', function () {
             it('Method play should throw an exception', function () {
-                expect(player.play).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.play).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method pause should throw an exception', function () {
-                expect(player.pause).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.pause).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method isPaused should throw an exception', function () {
-                expect(player.isPaused).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.isPaused).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method seek should throw an exception', function () {
-                expect(player.seek).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.seek).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method isSeeking should throw an exception', function () {
-                expect(player.isSeeking).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.isSeeking).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method isDynamic should throw an exception', function () {
-                expect(player.isDynamic).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.isDynamic).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method setPlaybackRate should throw an exception', function () {
-                expect(player.setPlaybackRate).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.setPlaybackRate).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method getPlaybackRate should throw an exception', function () {
-                expect(player.getPlaybackRate).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.getPlaybackRate).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method setMute should throw an exception', function () {
-                expect(player.setMute).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.setMute).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method isMuted should throw an exception', function () {
-                expect(player.isMuted).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.isMuted).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method setVolume should throw an exception', function () {
-                expect(player.setVolume).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.setVolume).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method getVolume should throw an exception', function () {
-                expect(player.getVolume).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.getVolume).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method time should throw an exception', function () {
-                expect(player.time).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.time).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method duration should throw an exception', function () {
-                expect(player.duration).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.duration).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method timeAsUTC should throw an exception', function () {
-                expect(player.timeAsUTC).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.timeAsUTC).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method durationAsUTC should throw an exception', function () {
-                expect(player.durationAsUTC).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.durationAsUTC).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
         });
 
@@ -498,15 +494,15 @@ describe('MediaPlayer', function () {
 
         describe('When it is not initialized', function () {
             it('Method getQualityFor should throw an exception', function () {
-                expect(player.getQualityFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getQualityFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method setQualityFor should throw an exception', function () {
-                expect(player.setQualityFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.setQualityFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method getInitialBitrateFor should throw an exception', function () {
-                expect(player.getInitialBitrateFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getInitialBitrateFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
         });
 
@@ -877,7 +873,7 @@ describe('MediaPlayer', function () {
 
         describe('When it is not initialized', function () {
             it('Method setTextTrack should throw an exception', function () {
-                expect(player.setTextTrack).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.setTextTrack).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
         });
     });
@@ -885,19 +881,19 @@ describe('MediaPlayer', function () {
     describe('Video Element Management Functions', function () {
         describe('When it is not initialized', function () {
             it('Method attachView should throw an exception when attaching a view', function () {
-                expect(player.attachView).to.throw(NOT_INITIALIZED_ERROR_MSG);
+                expect(player.attachView).to.throw(MediaPlayer.NOT_INITIALIZED_ERROR_MSG);
             });
 
             it('Method getVideoElement should throw an exception', function () {
-                expect(player.getVideoElement).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.getVideoElement).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method attachVideoContainer should throw an exception', function () {
-                expect(player.getVideoElement).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.getVideoElement).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
 
             it('Method attachTTMLRenderingDiv should throw an exception', function () {
-                expect(player.getVideoElement).to.throw(ELEMENT_NOT_ATTACHED_ERROR);
+                expect(player.getVideoElement).to.throw(MediaPlayer.ELEMENT_NOT_ATTACHED_ERROR);
             });
         });
         describe('When it is initialized', function () {
@@ -967,27 +963,27 @@ describe('MediaPlayer', function () {
     describe('Stream and Track Management Functions', function () {
         describe('When it is not initialized', function () {
             it('Method getBitrateInfoListFor should throw an exception', function () {
-                expect(player.getBitrateInfoListFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getBitrateInfoListFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method getStreamsFromManifest should throw an exception', function () {
-                expect(player.getStreamsFromManifest).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getStreamsFromManifest).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method getTracksFor should throw an exception', function () {
-                expect(player.getTracksFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getTracksFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method getTracksForTypeFromManifest should throw an exception', function () {
-                expect(player.getTracksForTypeFromManifest).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getTracksForTypeFromManifest).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method getCurrentTrackFor should throw an exception', function () {
-                expect(player.getCurrentTrackFor).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.getCurrentTrackFor).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
 
             it('Method setCurrentTrack should throw an exception', function () {
-                expect(player.setCurrentTrack).to.throw(PLAYBACK_NOT_INITIALIZED_ERROR);
+                expect(player.setCurrentTrack).to.throw(MediaPlayer.PLAYBACK_NOT_INITIALIZED_ERROR);
             });
         });
 
@@ -1062,7 +1058,7 @@ describe('MediaPlayer', function () {
     describe('Tools Functions', function () {
         describe('When it is not initialized', function () {
             it('Method attachSource should throw an exception', function () {
-                expect(player.attachSource).to.throw(NOT_INITIALIZED_ERROR_MSG);
+                expect(player.attachSource).to.throw(MediaPlayer.NOT_INITIALIZED_ERROR_MSG);
             });
         });
     });
