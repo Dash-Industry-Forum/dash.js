dd5c4264f8fd62ba620a6d3f64dc1fcfba4da675
Nicolas ANGOT <nicolas.angot@orange.com>
2017-10-04 14:41:23 +0200

    update module management in order to call 'dashjs.FactorMaker' in reporting and protection files.

--- index.js
+++ index.js
@@ -29,27 +29,15 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-import MediaPlayer from './src/streaming/MediaPlayer';
-import FactoryMaker from './src/core/FactoryMaker';
-import Protection from './src/streaming/protection/Protection';
+import './index_mediaplayerOnly';
+
 import MetricsReporting from './src/streaming/metrics/MetricsReporting';
+import Protection from './src/streaming/protection/Protection';
 import MediaPlayerFactory from './src/streaming/MediaPlayerFactory';
-import {getVersionString} from './src/core/Version';
 
-// Shove both of these into the global scope
-var context = (typeof window !== 'undefined' && window) || global;
-
-var dashjs = context.dashjs;
-if (!dashjs) {
-    dashjs = context.dashjs = {};
-}
-
-dashjs.MediaPlayer = MediaPlayer;
-dashjs.FactoryMaker = FactoryMaker;
 dashjs.Protection = Protection;
 dashjs.MetricsReporting = MetricsReporting;
 dashjs.MediaPlayerFactory = MediaPlayerFactory;
-dashjs.Version = getVersionString();
 
 export default dashjs;
-export { MediaPlayer, Protection, MetricsReporting, MediaPlayerFactory};
+export { Protection, MetricsReporting, MediaPlayerFactory};
