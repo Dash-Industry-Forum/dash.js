07bd80689309e832af11b8d6b0af617be33213e6
Jeff Cunat <jeanfrancois.cunat@orange.com>
2017-10-11 14:17:44 +0200

    jshint

--- test/unit/streaming.protection.drm.KeySystemPlayReady.js
+++ test/unit/streaming.protection.drm.KeySystemPlayReady.js
@@ -1,20 +1,18 @@
 import KeySystemPlayReady from '../../src/streaming/protection/drm/KeySystemPlayReady.js';
 
 const expect = require('chai').expect;
-const fs = require('fs');
-const domParser = require('xmldom').DOMParser;
 
-describe('KeySystemPlayready', function(){
+describe('KeySystemPlayready', function () {
 
     let keySystem;
     let cdmData = null;
 
     const protData = {
         cdmData: '2lfuDn3JoEo0dM324cA5tSv1gNNw65mgysBqNJqtxGUk7ShUOE03N6LK0cryu2roCQtDghmF7cC6xyt1WTA86CmrUNFRjo1tcxQtTVEW9Xw68pH7/yU2GbtK4zbctx49sffi4fYy8fGEUB5079CesBONxoKli5j2ADM8CWz93a5mYegZWraOq3EH0nvwvRXZ'
-    }
+    };
 
     const expectedCDMData = '<PlayReadyCDMData type="LicenseAcquisition"><LicenseAcquisition version="1.0" Proactive="false"><CustomData encoding="base64encoded"></CustomData></LicenseAcquisition></PlayReadyCDMData>';
-    
+
     const context = {};
 
     it('should exist', () => {
@@ -25,10 +23,10 @@ describe('KeySystemPlayready', function(){
     keySystem.init(protData);
     cdmData = keySystem.getCDMData();
 
-    it('should return the correct cdmData', function() {
-        expect(keySystem).to.be.not.undefined;
-        expect(cdmData).to.be.not.null;
+    it('should return the correct cdmData', function () {
+        expect(keySystem).to.be.defined; // jshint ignore:line
+        expect(cdmData).to.be.not.null; // jshint ignore:line
         expect(cdmData).to.equal(expectedCDMData);
     });
 
-})
+});
