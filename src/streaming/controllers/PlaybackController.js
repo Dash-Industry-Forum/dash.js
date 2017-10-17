062d10e80c8ec51c5bdfc0c151de71c826facfc3
Nicolas ANGOT <nicolas.angot@orange.com>
2017-10-17 11:24:57 +0200

    update getStreamStartTime and setLiveEdgeSeekTarget functions in order to fix issue with live stream with a startTimeOffset anchor

--- src/streaming/controllers/PlaybackController.js
+++ src/streaming/controllers/PlaybackController.js
@@ -239,10 +239,11 @@ function PlaybackController() {
 
     /**
      * @param {boolean} ignoreStartOffset - ignore URL fragment start offset if true
+     * @param {number} liveEdge - liveEdge value
      * @returns {number} object
      * @memberof PlaybackController#
      */
-    function getStreamStartTime(ignoreStartOffset) {
+    function getStreamStartTime(ignoreStartOffset, liveEdge) {
         let presentationStartTime;
         const fragData = URIQueryAndFragmentModel(context).getInstance().getURIFragmentData();
         let startTimeOffset = NaN;
@@ -263,7 +264,7 @@ function PlaybackController() {
                 presentationStartTime = startTimeOffset - (streamInfo.manifestInfo.availableFrom.getTime() / 1000);
 
                 if (presentationStartTime > liveStartTime ||
-                    presentationStartTime < (liveStartTime - streamInfo.manifestInfo.DVRWindowSize)) {
+                    presentationStartTime < (!isNaN(liveEdge) ? (liveEdge - streamInfo.manifestInfo.DVRWindowSize) : NaN)) {
                     presentationStartTime = null;
                 }
             }
