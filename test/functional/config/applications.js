399974b9956a5074b2c4a4012cfd6eb4616fe188
Nicolas ANGOT <nicolas.angot@orange.com>
2017-10-19 14:26:22 +0200

    add remote_all and remote_mss to test modules from BrowserStack. RemoteDashJSServer needs to be changed.

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,4 +1,6 @@
 define({
 	local_all:'http://localhost:8086/dash.js-OrangeOpenSource/test/functional/tests.html',
-    local_mss:'http://localhost:8086/dash.js-OrangeOpenSource/test/functional/tests.html?mss=true'
+    local_mss:'http://localhost:8086/dash.js-OrangeOpenSource/test/functional/tests.html?mss=true',
+	remote_all:'http://RemoteDashJSServer/test/functional/tests.html',
+    remote_mss:'http://RemoteDashJSServer/test/functional/tests.html?mss=true'
 });
