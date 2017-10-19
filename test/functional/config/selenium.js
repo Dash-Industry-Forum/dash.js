cc424b6b7e2382dd957f919a1ecee166fdcbc910
Nicolas ANGOT <nicolas.angot@orange.com>
2017-10-19 14:27:42 +0200

    add BrowserStack configuration for selenium

--- test/functional/config/selenium.js
+++ test/functional/config/selenium.js
@@ -14,5 +14,23 @@ define({
             'selenium-version': '3.4.0'
         },
         leaveRemoteOpen:'fail'
+    },
+
+    remote: {
+
+        capabilities: {
+            name: 'Tests DashJS',
+            build: process.env.BROWSERSTACK_LOCAL_IDENTIFIER || 'BROWSERSTACK_LOCAL_IDENTIFIER',
+            "browserstack.local": false,
+            "browserstack.debug": true,
+            fixSessionCapabilities: false
+        },
+        tunnel: 'BrowserStackTunnel',
+        tunnelOptions: {
+            verbose: true,
+            username: process.env.BROWSERSTACK_USER || 'BROWSERSTACK_USER',
+            accessKey: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY'
+        },
+        reporters: ['Runner', {id: 'JUnit', filename: 'test/functional/test-reports/' + (new Date().getFullYear())+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate())+'_'+(new Date().getHours())+'-'+(new Date().getMinutes())+'-'+(new Date().getSeconds()) + '_report.xml'}]
     }
 });
