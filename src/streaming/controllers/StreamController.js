84a55fdfff0d94241c2b8abdcef61989f5e218f4
Nicolas ANGOT <nicolas.angot@orange.com>
2018-01-11 14:39:15 +0100

    when a period is over, dash.js has to reset audioTrackDetected and  videoTrackDetected. Audio and video have to be detected when a new stream is activated.

--- src/streaming/controllers/StreamController.js
+++ src/streaming/controllers/StreamController.js
@@ -265,6 +265,8 @@ function StreamController() {
     function onEnded() {
         const nextStream = getNextStream();
         if (nextStream) {
+            audioTrackDetected = undefined;
+            videoTrackDetected = undefined;
             switchStream(activeStream, nextStream, NaN);
         }
         else {
@@ -339,6 +341,9 @@ function StreamController() {
 
         activeStream.activate(mediaSource);
 
+        isAudioTrackPresent();
+        isVideoTrackPresent();
+
         if (!initialPlayback) {
             if (!isNaN(seekTime)) {
                 playbackController.seek(seekTime); //we only need to call seek here, IndexHandlerTime was set from seeking event
@@ -348,8 +353,6 @@ function StreamController() {
                     adapter.setIndexHandlerTime(p, startTime);
                 });
             }
-        } else {
-            videoTrackDetected = checkTrackPresence(Constants.VIDEO);
         }
 
         activeStream.startEventController();
