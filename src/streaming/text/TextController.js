4ad0dc45b2f436c60732353eaf9329131fca48ad
Nicolas ANGOT <nicolas.angot@orange.com>
2018-03-27 11:37:52 +0200

    use some function instead of findIndex

--- src/streaming/text/TextController.js
+++ src/streaming/text/TextController.js
@@ -73,7 +73,6 @@ function TextController() {
         eventBus.on(Events.TEXT_TRACKS_QUEUE_INITIALIZED, onTextTracksAdded, instance);
 
         resetInitialSettings();
-        polyFillFindIndex();
     }
 
     function setConfig(config) {
@@ -122,53 +121,6 @@ function TextController() {
         });
     }
 
-    function polyFillFindIndex () {
-        //polyfill function for IE support
-        if (!Array.prototype.findIndex) {
-            Object.defineProperty(Array.prototype, 'findIndex', {
-                value: function (predicate) {
-                    // 1. Let O be ? ToObject(this value).
-                    if (this === null) {
-                        throw new TypeError('"this" is null or not defined');
-                    }
-
-                    var o = Object(this);
-
-                    // 2. Let len be ? ToLength(? Get(O, "length")).
-                    var len = o.length >>> 0;
-
-                    // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-                    if (typeof predicate !== 'function') {
-                        throw new TypeError('predicate must be a function');
-                    }
-
-                    // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-                    var thisArg = arguments[1];
-
-                    // 5. Let k be 0.
-                    var k = 0;
-
-                    // 6. Repeat, while k < len
-                    while (k < len) {
-                        // a. Let Pk be ! ToString(k).
-                        // b. Let kValue be ? Get(O, Pk).
-                        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-                        // d. If testResult is true, return k.
-                        var kValue = o[k];
-                        if (predicate.call(thisArg, kValue, k, o)) {
-                            return k;
-                        }
-                        // e. Increase k by 1.
-                        k++;
-                    }
-
-                    // 7. Return -1.
-                    return -1;
-                }
-            });
-        }
-    }
-
     function getTextSourceBuffer() {
         return textSourceBuffer;
     }
@@ -197,15 +149,13 @@ function TextController() {
         let tracks = e.tracks;
         let index = e.index;
 
-        // find track corresponding to default subtitle and apply it
-        let defaultLanguageIndex = tracks.findIndex((item) => {
-            return (item.lang === defaultLanguage);
+        tracks.some((item, idx) => {
+            if (item.lang === defaultLanguage) {
+                this.setTextTrack(idx);
+                index = idx;
+                return true;
+            }
         });
-
-        if (defaultLanguageIndex !== -1) {
-            this.setTextTrack(defaultLanguageIndex);
-            index = defaultLanguageIndex;
-        }
 
         if (!textDefaultEnabled) {
             // disable text at startup
