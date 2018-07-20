21cb800435fbb40435345dd09c95c595cfacf526
Bertrand Berthelot <bertrand.berthelot@orange.com>
2018-07-20 16:16:18 +0200

    MediaPlayer: add BaseURLController in MssHandler config

--- src/streaming/MediaPlayer.js
+++ src/streaming/MediaPlayer.js
@@ -34,6 +34,7 @@ import UTCTiming from '../dash/vo/UTCTiming';
 import PlaybackController from './controllers/PlaybackController';
 import StreamController from './controllers/StreamController';
 import MediaController from './controllers/MediaController';
+import BaseURLController from './controllers/BaseURLController';
 import ManifestLoader from './ManifestLoader';
 import ErrorHandler from './utils/ErrorHandler';
 import Capabilities from './utils/Capabilities';
@@ -2801,6 +2802,7 @@ function MediaPlayer() {
                 metricsModel: metricsModel,
                 playbackController: playbackController,
                 protectionController: protectionController,
+                baseURLController: BaseURLController(context).getInstance(),
                 errHandler: errHandler,
                 events: Events,
                 constants: Constants,
