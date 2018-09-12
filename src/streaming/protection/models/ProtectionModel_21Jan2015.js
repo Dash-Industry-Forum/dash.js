df0e882b16d7853fe17bd8cad68e12bc33c260b5
Bertrand Berthelot <bertrand.berthelot@orange.com>
2018-09-12 11:10:08 +0200

    ProtectionModel_21Jan2015: logs

--- src/streaming/protection/models/ProtectionModel_21Jan2015.js
+++ src/streaming/protection/models/ProtectionModel_21Jan2015.js
@@ -341,7 +341,6 @@ function ProtectionModel_21Jan2015(config) {
         for (let i = 0; i < sessions.length; i++) {
             if (sessions[i] === token) {
                 sessions.splice(i,1);
-                logger.debug('DRM: Session removed.  SessionID = ' + token.getSessionID());
                 break;
             }
         }
