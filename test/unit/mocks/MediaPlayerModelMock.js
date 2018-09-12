af228ea79f435fc2f06b3c924dbd55bf2bd7f606
Nicolas ANGOT <nicolas.angot@orange.com>
2018-09-12 14:18:39 +0200

    update default timing source

--- test/unit/mocks/MediaPlayerModelMock.js
+++ test/unit/mocks/MediaPlayerModelMock.js
@@ -36,7 +36,7 @@ import Constants from '../../../src/streaming/constants/Constants';
 
 const DEFAULT_UTC_TIMING_SOURCE = {
     scheme: 'urn:mpeg:dash:utc:http-xsdate:2014',
-    value: 'http://time.akamai.com/?iso'
+    value: 'http://time.akamai.com/?iso&ms'
 };
 const LIVE_DELAY_FRAGMENT_COUNT = 4;
 
