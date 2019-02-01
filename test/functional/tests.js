c6000c57fdb0b5c27543e09bb6f21c50b062be22
Bertrand Berthelot <bertrand.berthelot@orange.com>
2019-02-01 15:34:36 +0100

    add ended event test

--- test/functional/tests.js
+++ test/functional/tests.js
@@ -4,22 +4,25 @@ define([
     'test/functional/tests/setup',
     'test/functional/tests/play',
     'test/functional/tests/pause',
-    'test/functional/tests/seek'
+    'test/functional/tests/seek',
+    'test/functional/tests/ended',
 ], function(intern,
             streams,
             setup,
             play,
             pause,
-            seek) {
+            seek,
+            ended) {
 
     var registerSuites = function (stream) {
-        var suites = intern.config.suites || ['play', 'pause', 'seek'];
+        var suites = intern.config.suites || ['play', 'pause', 'seek', 'ended'];
 
         setup.register(stream);
 
         if (suites.indexOf('play') !== -1) play.register(stream);
         if (suites.indexOf('pause') !== -1) pause.register(stream);
         if (suites.indexOf('seek') !== -1) seek.register(stream);
+        if (suites.indexOf('ended') !== -1) ended.register(stream);
     };
 
     for (var i = 0; i < streams.items.length; i++) {
