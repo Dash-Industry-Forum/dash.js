dc22608884ed3af9bbbf3e603368ce9cf5ad1ccc
Bertrand Berthelot <bertrand.berthelot@orange.com>
2019-02-04 17:48:03 +0100

    circleci

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,4 +1,4 @@
 define({
     local:'localhost/DEV/Orange-OpenSource/dash.js/test/functional/tests.html',
-    remote:'reference.dashif.org/dash.js/nightly/test/functional/tests.html',
+    remote:'orange-opensource.github.io/dash.js/test/functional/tests.html',
 });
