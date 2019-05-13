715af3a675e4224007ea9b2660aadc4ccda0e43d
Bertrand Berthelot <bertrand.berthelot@orange.com>
2019-05-13 15:52:39 +0200

    functional tests integration with circleci

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,4 +1,4 @@
 define({
-    local:'localhost/DEV/Orange-OpenSource/dash.js/test/functional/tests.html',
-    remote:'orange-opensource.github.io/dash.js/test/functional/tests.html',
+    local:'localhost:3000/samples/functional-tests/index.html',
+    remote:'reference.dashif.org/dash.js/nightly/samples/functional-tests/index.html',
 });
