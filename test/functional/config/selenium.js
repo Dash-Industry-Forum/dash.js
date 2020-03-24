21d6e7f39e512423cff6228e371692e4ea6a724d
Bertrand Berthelot <bertrand.berthelot@orange.com>
2020-03-24 08:58:27 +0100

    circleci: set output functional tests results filename

--- test/functional/config/selenium.js
+++ test/functional/config/selenium.js
@@ -13,7 +13,7 @@ define({
             'Runner',
             {
                 id: 'JUnit',
-                filename: 'test/functional/test-reports/junit/results_' + (new Date().getFullYear())+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate())+'_'+(new Date().getHours())+'-'+(new Date().getMinutes()) + '.xml'
+                filename: 'test/functional/reports/junit/results_' + (new Date().getFullYear())+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate())+'_'+(new Date().getHours())+'-'+(new Date().getMinutes()) + '.xml'
             }
         ],
         capabilities: {
@@ -41,7 +41,7 @@ define({
             'Runner',
             {
                 id: 'JUnit',
-                filename: 'test/functional/test-reports/junit/results_' + (new Date().getFullYear())+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate())+'_'+(new Date().getHours())+'-'+(new Date().getMinutes()) + '.xml'
+                filename: 'test/functional/reports/junit/results_' + (new Date().getFullYear())+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate())+'_'+(new Date().getHours())+'-'+(new Date().getMinutes()) + '.xml'
             }
         ]
     }
