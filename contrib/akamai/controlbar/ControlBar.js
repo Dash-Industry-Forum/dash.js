fe337f9833d214d8af2e990cd6780f04d567a030
Ashutosh Singh <40604544+ashutoshsingh0223@users.noreply.github.com>
2020-06-24 17:47:08 +0530

    exitFullscreen ->  fullscreenElement (#3296)
    * exitFullscreen ->  fullscreenElement
    
    document.exitFullscreen ->  document.fullscreenElement in exitFullscreen.
    
    * Add old `exitFullscreen` functionality

--- contrib/akamai/controlbar/ControlBar.js
+++ contrib/akamai/controlbar/ControlBar.js
@@ -433,8 +433,10 @@ var ControlBar = function (dashjsMediaPlayer, displayUTCTimeCodes) {
         window.removeEventListener('mousemove', onFullScreenMouseMove);
         clearFullscreenState();
 
-        if (document.exitFullscreen) {
+        if (document.fullscreenElement) {
             document.exitFullscreen();
+        } else if (document.exitFullscreen) {
+           document.exitFullscreen(); 
         } else if (document.mozCancelFullScreen) {
             document.mozCancelFullScreen();
         } else if (document.msExitFullscreen) {
