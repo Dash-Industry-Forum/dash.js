7f9a123310f4d86331ed8b8f575912e28a9e04ab
Martin Lasak <martin.lasak@fokus.fraunhofer.de>
2020-06-30 16:48:39 +0200

    Fix no-unused-vars

--- src/mss/MssHandler.js
+++ src/mss/MssHandler.js
@@ -36,7 +36,6 @@ import MssFragmentProcessor from './MssFragmentProcessor';
 import MssParser from './parser/MssParser';
 import MssErrors from './errors/MssErrors';
 import DashJSError from '../streaming/vo/DashJSError';
-import InitCache from '../streaming/utils/InitCache';
 
 function MssHandler(config) {
 
@@ -63,12 +62,10 @@ function MssHandler(config) {
     });
     let mssParser,
         fragmentInfoControllers,
-        initCache,
         instance;
 
     function setup() {
         fragmentInfoControllers = [];
-        initCache = InitCache(context).getInstance();
     }
 
     function getStreamProcessor(type) {
