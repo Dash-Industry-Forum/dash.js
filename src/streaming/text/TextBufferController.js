6104e0275a7600dde1bfefd3db3c8c16ba9e131a
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2020-08-11 08:02:57 +0200

    Add availabilityWindow to TextBufferController.js

--- src/streaming/text/TextBufferController.js
+++ src/streaming/text/TextBufferController.js
@@ -83,8 +83,8 @@ function TextBufferController(config) {
         return _BufferControllerImpl.initialize(source, StreamProcessor);
     }
 
-    function createBuffer(mediaInfoArr) {
-        return _BufferControllerImpl.createBuffer(mediaInfoArr);
+    function createBuffer(mediaInfoArr, previousBuffers, streamInfo) {
+        return _BufferControllerImpl.createBuffer(mediaInfoArr, previousBuffers, streamInfo);
     }
 
     function getType() {
