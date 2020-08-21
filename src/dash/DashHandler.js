9cb03b701d6eafc701a71c0ad0c0193646134280
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2020-08-21 11:54:40 +0200

    Fix live segment list (#3369)
    * Fix a bug in the ListSegmentsGetter.js when calculating the right index for a specific time. startnumber needs to be considered
    
    * Fix a bug in SegmentsUtils.js. Startnumber needs to be considered when calculating the presentationStartTime
    
    * Remove console logs
    
    * Fix wrong offset in ListSegmentsGetter.js
    
    * Add period start to range calculation for segment timeline and no tsbd
    
    * Always calculate the availabilityRange for segmentTimeline live manifests based on the segments defined in the MPD
    
    * Use PTO for correct calculation of segment availability start time when using SegmentTimeline
    
    * Use PTO for correct calculation of segment availability start time when using SegmentTimeline
    
    * Only adjust current time when DVR window is larger than 4 seconds
    
    * Add presentationTimeOffset to getIndexBasedSegment
    
    * Additional fixes for timing calculation
    
    * Specian treatment for SegmentList
    
    * Solve SegmentList problem much more elegant by using index numbers relative to period@start

--- src/dash/DashHandler.js
+++ src/dash/DashHandler.js
@@ -29,7 +29,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 import FragmentRequest from '../streaming/vo/FragmentRequest';
-import { HTTPRequest } from '../streaming/vo/metrics/HTTPRequest';
+import {HTTPRequest} from '../streaming/vo/metrics/HTTPRequest';
 import FactoryMaker from '../core/FactoryMaker';
 import {
     replaceIDForTemplate,
@@ -93,11 +93,11 @@ function DashHandler(config) {
         return streamInfo;
     }
 
-    function setCurrentIndex (value) {
+    function setCurrentIndex(value) {
         segmentIndex = value;
     }
 
-    function getCurrentIndex () {
+    function getCurrentIndex() {
         return segmentIndex;
     }
 
@@ -196,7 +196,10 @@ function DashHandler(config) {
         //if representation has initialization and segments information, REPRESENTATION_UPDATE_COMPLETED can be triggered immediately
         //otherwise, it means that a request has to be made to get initialization and/or segments informations
         if (hasInitialization && hasSegments) {
-            eventBus.trigger(events.REPRESENTATION_UPDATE_COMPLETED, {sender: instance, representation: voRepresentation});
+            eventBus.trigger(events.REPRESENTATION_UPDATE_COMPLETED, {
+                sender: instance,
+                representation: voRepresentation
+            });
         } else {
             segmentsController.update(voRepresentation, getType(), selectedMimeType, hasInitialization, hasSegments);
         }
@@ -209,8 +212,7 @@ function DashHandler(config) {
 
         const request = new FragmentRequest();
         const representation = segment.representation;
-        const bandwidth = representation.adaptation.period.mpd.manifest.Period_asArray[representation.adaptation.period.index].
-            AdaptationSet_asArray[representation.adaptation.index].Representation_asArray[representation.index].bandwidth;
+        const bandwidth = representation.adaptation.period.mpd.manifest.Period_asArray[representation.adaptation.period.index].AdaptationSet_asArray[representation.adaptation.index].Representation_asArray[representation.index].bandwidth;
         let url = segment.media;
 
         url = replaceTokenForTemplate(url, 'Number', segment.replacementNumber);
@@ -312,9 +314,9 @@ function DashHandler(config) {
 
         requestedTime = null;
 
-        const indexToRequest = segmentIndex + 1;
-        logger.debug('Getting the next request at index: ' + indexToRequest);
+        let indexToRequest = segmentIndex + 1;
 
+        logger.debug('Getting the next request at index: ' + indexToRequest);
         // check that there is a segment in this index
         const segment = segmentsController.getSegmentByIndex(representation, indexToRequest, lastSegment ? lastSegment.mediaStartTime : -1);
         if (!segment && isEndlessMedia(representation) && !dynamicStreamCompleted) {
@@ -396,7 +398,10 @@ function DashHandler(config) {
         }
 
         if (segments.length > 0) {
-            representation.segmentAvailabilityRange = {start: segments[0].presentationStartTime, end: segments[segments.length - 1].presentationStartTime};
+            representation.segmentAvailabilityRange = {
+                start: segments[0].presentationStartTime,
+                end: segments[segments.length - 1].presentationStartTime
+            };
             representation.availableSegmentsNumber = segments.length;
             representation.segments = segments;
 
