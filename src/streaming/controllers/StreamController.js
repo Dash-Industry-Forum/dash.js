20a1ff12cafe924bf03ec35a3d0b1ad84109766f
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2020-09-11 11:25:20 +0200

    Fix prebuffering static mpds (#3405)
    * Check if stream is dynamic when trying to prebuffer
    
    * Check if stream is dynamic when trying to prebuffer

--- src/streaming/controllers/StreamController.js
+++ src/streaming/controllers/StreamController.js
@@ -282,7 +282,8 @@ function StreamController() {
                         const mediaInfo = adapter.getMediaInfoForType(stream.getStreamInfo(), mediaType);
                         const voRepresentations = adapter.getVoRepresentations(mediaInfo);
                         voRepresentations.forEach((voRep) => {
-                            const range = timelineConverter.calcSegmentAvailabilityRange(voRep, true);
+                            const isDynamic = adapter.getIsDynamic();
+                            const range = timelineConverter.calcSegmentAvailabilityRange(voRep, isDynamic);
 
                             if (range.end < range.start) {
                                 segmentAvailabilityRangeIsOk = false;
