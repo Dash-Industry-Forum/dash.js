99bb473b45861935e10aa420029f81261cf41c01
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2020-10-05 13:25:05 +0200

    Update StreamMock.js

--- test/unit/mocks/StreamMock.js
+++ test/unit/mocks/StreamMock.js
@@ -1,21 +1,39 @@
+import AdapterMock from './AdapterMock';
+
 function StreamMock () {
 
     this.streamInfo = {};
+    this.dashAdapter = new AdapterMock();
+}
 
+StreamMock.prototype.getStreamInfo = function () {
+    return this.streamInfo;
+};
 
-    this.getStreamInfo = function () {
-        return this.streamInfo;
-    };
+StreamMock.prototype.setStreamInfo = function (streamInfo) {
+    this.streamInfo = streamInfo;
+};
 
-    this.setStreamInfo = function (streamInfo) {
-        this.streamInfo = streamInfo;
-    };
-
-    this.getFragmentController = function () {
-        return { getModel: () => {
+StreamMock.prototype.getFragmentController = function () {
+    return { getModel: () => {
             return { setStreamProcessor: () => {} };
         } };
-    };
-}
+};
+
+StreamMock.prototype.getAdapter = function () {
+    return this.dashAdapter;
+};
+
+StreamMock.prototype.setRegularPeriods = function (periods) {
+   this.dashAdapter.setRegularPeriods(periods);
+};
+
+StreamMock.prototype.setRepresentation = function (representation) {
+    this.dashAdapter.setRepresentation(representation);
+};
+
+
+
+
 
 export default StreamMock;
