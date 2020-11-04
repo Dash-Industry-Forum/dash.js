a8399dfd209967e3f92980676109344e2134e0c0
Bertrand Berthelot <bertrand.berthelot@orange.com>
2020-11-04 08:47:43 +0100

    Revert "run functional tests"
    This reverts commit e6ddd348a642942d008a7a845c6a1e75e7836c4f.

--- test/functional/config/applications.js
+++ test/functional/config/applications.js
@@ -1,4 +1,4 @@
 define({
     local:'http://localhost:3000/samples/functional-tests/index.html',
-    remote:'reference.dashif.org/dash.js/pull/3372/samples/functional-tests/index.html',
+    remote:'orange-opensource.github.io/dash.js/pr3372/samples/functional-tests/index.html',
 });
