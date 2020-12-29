602b7c4831a0f62e5b815d5f13fd391ccc2d8d49
Bertrand Berthelot <bertrand.berthelot@orange.com>
2020-12-29 17:36:01 +0100

    - rename test "audioSwitch" - get list of audio tracks at stream setup (see "play" test) - skip "audioSwitch" test if no more than 1 audio track - check if playing after each audio track switch

--- test/functional/testSuites.js
+++ test/functional/testSuites.js
@@ -8,11 +8,11 @@ const playFromTime = require('./tests/playFromTime');
 const pause = require('./tests/pause');
 const seek = require('./tests/seek');
 const seekPeriods = require('./tests/seekPeriods');
-const audioSwitching = require('./tests/audioSwitching');
+const audioSwitch = require('./tests/audioSwitch');
 const ended = require('./tests/ended');
 
 var registerSuites = function (stream) {
-    var suites = intern.config.testSuites || ['playFromTime', 'pause', 'seek', 'seekPeriods', 'audioSwitching', 'ended'];
+    var suites = intern.config.testSuites || ['playFromTime', 'pause', 'seek', 'seekPeriods', 'audioSwitch', 'ended'];
 
     setup.register(stream);
     play.register(stream);
@@ -21,7 +21,7 @@ var registerSuites = function (stream) {
     if (suites.indexOf('pause') !== -1) pause.register(stream);
     if (suites.indexOf('seek') !== -1) seek.register(stream);
     if (suites.indexOf('seek') !== -1) seekPeriods.register(stream);
-    if (suites.indexOf('audioSwitching') !== -1) audioSwitching.register(stream);
+    if (suites.indexOf('audioSwitch') !== -1) audioSwitch.register(stream);
     if (suites.indexOf('ended') !== -1) ended.register(stream);
 };
 
