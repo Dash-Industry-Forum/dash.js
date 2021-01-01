e4f0d6cc8b8d2faf963c97450e6ed4dbd42cbee2
Vinay Rosenberg <vinay.rosenberg@fokus.fraunhofer.de>
2021-01-02 00:42:00 +0100

    feature initial Audio

---
+++ test/functional/tests/initialAudio.js
@@ -0,0 +1,60 @@
+/**
+INITIAL_AUDIO:
+
+- for each audio track:
+    - load test page
+    - set initial audio track
+    - load stream
+    - play stream
+    - check new current audio track
+    - check if playback progressing
+**/
+const intern = require('intern').default;
+const { suite, before, test, after} = intern.getPlugin('interface.tdd');
+const { assert } = intern.getPlugin('chai');
+
+const constants = require('./scripts/constants.js');
+const utils = require('./scripts/utils.js');
+const player = require('./scripts/player.js');
+
+// Suite name
+const NAME = 'INITIAL_AUDIO';
+
+// test constants
+const SWITCH_WAIT = 3;
+
+exports.register = function (stream) {
+
+    suite(utils.testName(NAME, stream), (suite) => {
+
+        before(async () => {
+            if (!stream.available) suite.skip();
+            utils.log(NAME, 'Load stream');
+            
+        });
+
+        test('switch audio track', async ({ remote }) => {
+            for (let i = 0; i < stream.audioTracks.length ; i++) {
+                // reload page
+                command = remote.get(intern.config.testPage);
+                await command.execute(player.setAutoPlay, [false]);
+
+                // set initial track
+                utils.log(NAME, 'set initial audio track: ' + stream.audioTracks[i].lang);
+                await command.execute(player.setInitialMediaSettingsFor, ['audio', {
+                    lang: stream.audioTracks[i].lang 
+                }]);
+                await command.execute(player.loadStream, [stream]);
+                await command.execute(player.play, []);
+                
+                // Wait
+                await command.sleep(SWITCH_WAIT * 1000);
+                
+                // Check if initial track is correct
+                const newTrack = await command.execute(player.getCurrentTrackFor, ['audio']);
+                utils.log(NAME, 'current audio track: ' + newTrack.lang);
+                assert.deepEqual(newTrack.lang, stream.audioTracks[i].lang);
+            }           
+        });
+    });
+}
