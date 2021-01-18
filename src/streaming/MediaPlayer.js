2ac7c59fb9bd94bbe35730d7a1e2d2853c10f946
Vinay Rosenberg <vinay.rosenberg@fokus.fraunhofer.de>
2021-01-18 14:21:45 +0100

    update docs

--- src/streaming/MediaPlayer.js
+++ src/streaming/MediaPlayer.js
@@ -1493,6 +1493,7 @@ function MediaPlayer() {
      * This method allows to set media settings that will be used to pick the initial track. Format of the settings
      * is following: <br />
      * {lang: langValue (can be either a string or a regex to match),
+     *  index: indexValue,
      *  viewpoint: viewpointValue,
      *  audioChannelConfiguration: audioChannelConfigurationValue,
      *  accessibility: accessibilityValue,
@@ -1518,6 +1519,7 @@ function MediaPlayer() {
      * This method returns media settings that is used to pick the initial track. Format of the settings
      * is following:
      * {lang: langValue,
+     *  index: indexValue,
      *  viewpoint: viewpointValue,
      *  audioChannelConfiguration: audioChannelConfigurationValue,
      *  accessibility: accessibilityValue,
