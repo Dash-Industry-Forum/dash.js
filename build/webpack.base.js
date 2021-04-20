34ea8b56e38741b0799ea73141181dde5b240b26
Bertrand Berthelot <bertrand.berthelot@orange.com>
2021-04-20 10:34:36 +0200

    Webpack (#3596)
    * Add webpack
    
    * Complete webpack
    Remove grunt
    
    * remove .jshintrc
    
    * remove jshint ignore:line
    
    * clean
    
    * package.json: fix main
    
    * use rimraf instrad of clean-webpack-plugin
    
    * keep bundle filenames as current ones
    
    * use node modules replace-in-file and ftp-deploy for ftp deployment
    
    * update circleci config
    
    * Replace version in src/core/Version.js with package version
    
    * .eslintrc
    
    * test ftp deploy
    
    * test ftp deploy
    
    * revert remove jshint ignore:line
    
    * remove
    
    * remove polyjuice dev dep
    
    * restore build of offline bundle
    
    * update deploy.js
    
    * update EADME
    
    * reorder package.json config
    
    * restore build of offline bundle
    
    * update githooks.js
    
    * fix deploy script
    
    * circle ci:restore deploy job

---
+++ build/webpack.base.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const pkg = require('../package.json');
+
+const out_dir = '../dist';
+
+const config = {
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, out_dir),
+        publicPath: '/dist/',
+        library: 'dashjs',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                loader: 'string-replace-loader',
+                options: {
+                    search: '__VERSION__',
+                    replace: pkg.version,
+                }
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: `babel-loader`,
+                        options: { presets: ['@babel/env'] }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ESLintPlugin({
+            files: [
+                'src/**/*.js',
+                'test/unit/mocks/*.js',
+                'test/unit/*.js'
+            ]
+        })
+    ]
+}
+
+module.exports = { config };
