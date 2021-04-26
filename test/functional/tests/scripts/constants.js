00ec08aae1447fb24ea04f58d4ba03857928112a
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2021-04-26 10:26:08 +0200

    Reduce PROGRESS_DELAY for functional tests

--- test/functional/tests/scripts/constants.js
+++ test/functional/tests/scripts/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     EVENT_TIMEOUT: 10,      // Timeout (in sec.) for receiving player or video element events
-    PROGRESS_DELAY: 5,      // Playback progress delay (in sec.) to be checked
+    PROGRESS_DELAY: 3,      // Playback progress delay (in sec.) to be checked
     SEEK_END_SHIFT: 10,     // Shift (in sec.) when seeking to end
     DURATION_TOLERANCE: 3   // Tolerance for duration (for seeking)
 }
