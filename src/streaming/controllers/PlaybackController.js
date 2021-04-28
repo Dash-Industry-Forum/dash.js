7f90a62b94070fd92644df39f9986b98e2cd2cf9
Bertrand Berthelot <bertrand.berthelot@orange.com>
2021-04-28 17:08:53 +0200

    Fix issue #3623

--- src/streaming/controllers/PlaybackController.js
+++ src/streaming/controllers/PlaybackController.js
@@ -396,7 +396,7 @@ function PlaybackController() {
         const currentTime = getNormalizedTime();
         const actualTime = getActualPresentationTime(currentTime, mediaType);
         const timeChanged = (!isNaN(actualTime) && actualTime !== currentTime);
-        if (timeChanged && isStalled()) {
+        if (timeChanged && !isSeeking() && isStalled()) {
             logger.debug(`UpdateCurrentTime: Seek to actual time: ${actualTime} from currentTime: ${currentTime}`);
             seek(actualTime);
         }
