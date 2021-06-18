aef98e044df730a0416374a086edd15fc63bdf0c
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2021-06-18 09:32:55 +0200

    Optimize ll testplayer

--- samples/low-latency/testplayer/main.js
+++ samples/low-latency/testplayer/main.js
@@ -1,18 +1,59 @@
 var METRIC_INTERVAL = 300;
-var CHART_INTERVAL = 500;
-var NUMBER_OF_CHART_ENTRIES = 30;
 
 var App = function () {
     this.player = null;
     this.video = null;
     this.chart = null;
+    this.domElements = {
+        settings: {},
+        metrics: {},
+        chart: {}
+    }
+    this.chartTimeout = null;
+    this.chartReportingInterval = 300;
+    this.chartNumberOfEntries = 30;
+    this.chartData = {
+        playbackTime: 0,
+        lastTimeStamp: null
+    }
 };
 
 App.prototype.init = function () {
+    this._setDomElements();
     this._adjustSettingsByUrlParameters();
     this._registerEventHandler();
     this._startIntervalHandler();
     this._setupLineChart();
+}
+
+App.prototype._setDomElements = function () {
+    this.domElements.settings.targetLatency = document.getElementById('target-latency');
+    this.domElements.settings.minDrift = document.getElementById('min-drift');
+    this.domElements.settings.maxDrift = document.getElementById('max-drift');
+    this.domElements.settings.catchupPlaybackRate = document.getElementById('catchup-playback-rate');
+    this.domElements.settings.liveCatchupLatencyThreshold = document.getElementById('catchup-threshold');
+    this.domElements.settings.abrAdditionalInsufficientBufferRule = document.getElementById('abr-additional-insufficient')
+    this.domElements.settings.abrAdditionalDroppedFramesRule = document.getElementById('abr-additional-dropped');
+    this.domElements.settings.abrAdditionalAbandonRequestRule = document.getElementById('abr-additional-abandon');
+    this.domElements.settings.abrAdditionalSwitchHistoryRule = document.getElementById('abr-additional-switch');
+    this.domElements.settings.targetLatency = document.getElementById('target-latency');
+    this.domElements.settings.exportSettingsUrl = document.getElementById('export-settings-url');
+
+    this.domElements.chart.metricChart = document.getElementById('metric-chart');
+    this.domElements.chart.enabled = document.getElementById('chart-enabled');
+    this.domElements.chart.interval = document.getElementById('chart-interval');
+    this.domElements.chart.numberOfEntries = document.getElementById('chart-number-of-entries');
+
+    this.domElements.metrics.latencyTag = document.getElementById('latency-tag');
+    this.domElements.metrics.mindriftTag = document.getElementById('mindrift-tag');
+    this.domElements.metrics.playbackrateTag = document.getElementById('playbackrate-tag');
+    this.domElements.metrics.bufferTag = document.getElementById('buffer-tag');
+    this.domElements.metrics.catchupThresholdTag = document.getElementById('catchup-threshold-tag');
+    this.domElements.metrics.sec = document.getElementById('sec');
+    this.domElements.metrics.min = document.getElementById('min');
+    this.domElements.metrics.videoMaxIndex = document.getElementById('video-max-index');
+    this.domElements.metrics.videoIndex = document.getElementById('video-index');
+    this.domElements.metrics.videoBitrate = document.getElementById('video-bitrate');
 }
 
 App.prototype._load = function () {
@@ -21,13 +62,15 @@ App.prototype._load = function () {
     if (this.player) {
         this.player.reset();
         this._unregisterDashEventHandler();
+        this.chartData.playbackTime = 0;
+        this.chartData.lastTimeStamp = null
     }
 
     url = document.getElementById('manifest').value;
 
     this.video = document.querySelector('video');
     this.player = dashjs.MediaPlayer().create();
-    this.player.updateSettings({ 'streaming': { 'lowLatencyEnabled': true } });
+    this.player.updateSettings({ streaming: { lowLatencyEnabled: true } });
     this.player.initialize(this.video, url, true);
     this._registerDashEventHandler();
     this._applyParameters();
@@ -77,42 +120,40 @@ App.prototype._exportSettings = function () {
         url += '&' + key + '=' + value
     }
 
-    document.getElementById('export-settings-url').value = encodeURI(url);
+    this.domElements.settings.exportSettingsUrl.value = encodeURI(url);
 }
 
 App.prototype._adjustSettingsByUrlParameters = function () {
     var urlSearchParams = new URLSearchParams(window.location.search);
     var params = Object.fromEntries(urlSearchParams.entries());
 
-    console.log(params);
-
     if (params) {
         if (params.targetLatency !== undefined) {
-            document.getElementById('target-latency').value = parseFloat(params.targetLatency).toFixed(1);
+            this.domElements.settings.targetLatency.value = parseFloat(params.targetLatency).toFixed(1);
         }
         if (params.maxDrift !== undefined) {
-            document.getElementById('max-drift').value = parseFloat(params.maxDrift).toFixed(1);
+            this.domElements.settings.maxDrift.value = parseFloat(params.maxDrift).toFixed(1);
         }
         if (params.minDrift !== undefined) {
-            document.getElementById('min-drift').value = parseFloat(params.minDrift).toFixed(2);
+            this.domElements.settings.minDrift.value = parseFloat(params.minDrift).toFixed(2);
         }
         if (params.catchupPlaybackRate !== undefined) {
-            document.getElementById('catchup-playback-rate').value = parseFloat(params.catchupPlaybackRate).toFixed(1);
+            this.domElements.settings.catchupPlaybackRate.value = parseFloat(params.catchupPlaybackRate).toFixed(1);
         }
         if (params.liveCatchupLatencyThreshold !== undefined) {
-            document.getElementById('catchup-threshold').value = parseFloat(params.liveCatchupLatencyThreshold).toFixed(0);
+            this.domElements.settings.liveCatchupLatencyThreshold.value = parseFloat(params.liveCatchupLatencyThreshold).toFixed(0);
         }
         if (params.abrAdditionalInsufficientBufferRule !== undefined) {
-            document.getElementById('abr-additional-insufficient').checked = params.abrAdditionalInsufficientBufferRule === 'true';
+            this.domElements.settings.abrAdditionalInsufficientBufferRule.checked = params.abrAdditionalInsufficientBufferRule === 'true';
         }
         if (params.abrAdditionalAbandonRequestRule !== undefined) {
-            document.getElementById('abr-additional-abandon').checked = params.abrAdditionalAbandonRequestRule === 'true';
+            this.domElements.settings.abrAdditionalAbandonRequestRule.checked = params.abrAdditionalAbandonRequestRule === 'true';
         }
         if (params.abrAdditionalSwitchHistoryRule !== undefined) {
-            document.getElementById('abr-additional-switch').checked = params.abrAdditionalSwitchHistoryRule === 'true';
+            this.domElements.settings.abrAdditionalSwitchHistoryRule.checked = params.abrAdditionalSwitchHistoryRule === 'true';
         }
         if (params.abrAdditionalDroppedFramesRule !== undefined) {
-            document.getElementById('abr-additional-dropped').checked = params.abrAdditionalDroppedFramesRule === 'true';
+            this.domElements.settings.abrAdditionalDroppedFramesRule.checked = params.abrAdditionalDroppedFramesRule === 'true';
         }
         if (params.abrGeneral !== undefined) {
             document.getElementById(params.abrGeneral).checked = true;
@@ -124,20 +165,20 @@ App.prototype._adjustSettingsByUrlParameters = function () {
             document.getElementById(params.throughputCalculation).checked = true;
         }
     }
-    //document.querySelector('input[name="gender"]:checked').value
+
 }
 
 App.prototype._getCurrentSettings = function () {
-    var targetLatency = parseFloat(document.getElementById('target-latency').value, 10);
-    var minDrift = parseFloat(document.getElementById('min-drift').value, 10);
-    var maxDrift = parseFloat(document.getElementById('max-drift').value, 10);
-    var catchupPlaybackRate = parseFloat(document.getElementById('catchup-playback-rate').value, 10);
-    var liveCatchupLatencyThreshold = parseFloat(document.getElementById('catchup-threshold').value, 10);
+    var targetLatency = parseFloat(this.domElements.settings.targetLatency.value, 10);
+    var minDrift = parseFloat(this.domElements.settings.minDrift.value, 10);
+    var maxDrift = parseFloat(this.domElements.settings.maxDrift.value, 10);
+    var catchupPlaybackRate = parseFloat(this.domElements.settings.catchupPlaybackRate.value, 10);
+    var liveCatchupLatencyThreshold = parseFloat(this.domElements.settings.liveCatchupLatencyThreshold.value, 10);
+    var abrAdditionalInsufficientBufferRule = this.domElements.settings.abrAdditionalInsufficientBufferRule.checked;
+    var abrAdditionalDroppedFramesRule = this.domElements.settings.abrAdditionalDroppedFramesRule.checked;
+    var abrAdditionalAbandonRequestRule = this.domElements.settings.abrAdditionalAbandonRequestRule.checked;
+    var abrAdditionalSwitchHistoryRule = this.domElements.settings.abrAdditionalSwitchHistoryRule.checked;
     var abrGeneral = document.querySelector('input[name="abr-general"]:checked').value;
-    var abrAdditionalInsufficientBufferRule = document.getElementById('abr-additional-insufficient').checked;
-    var abrAdditionalDroppedFramesRule = document.getElementById('abr-additional-dropped').checked;
-    var abrAdditionalAbandonRequestRule = document.getElementById('abr-additional-abandon').checked;
-    var abrAdditionalSwitchHistoryRule = document.getElementById('abr-additional-switch').checked;
     var catchupMechanism = document.querySelector('input[name="catchup"]:checked').value;
     var throughputCalculation = document.querySelector('input[name="throughput-calc"]:checked').value;
 
@@ -214,35 +255,53 @@ App.prototype._setupLineChart = function () {
             }
         },
     };
-    var playbackTime = 0,
-        lastTimeStamp = null;
-    var self = this;
 
     // eslint-disable-next-line no-undef
     this.chart = new Chart(
-        document.getElementById('metric-chart'),
+        this.domElements.chart.metricChart,
         config
     );
 
-    setInterval(function () {
+    this._enableChart(true);
+}
+
+App.prototype._enableChart = function (enabled) {
+    if (!enabled && this.chartTimeout) {
+        clearTimeout(this.chartTimeout);
+        this.chartTimeout = null;
+        return;
+    }
+
+    if (this.chartTimeout && enabled) {
+        return;
+    }
+
+    this._updateChartData();
+
+}
+
+App.prototype._updateChartData = function () {
+    var self = this;
+
+    this.chartTimeout = setTimeout(function () {
         if (self.player && self.player.isReady()) {
             const data = self.chart.data;
             if (data.datasets.length > 0) {
 
-                if (data.labels.length > NUMBER_OF_CHART_ENTRIES) {
+                if (data.labels.length > self.chartNumberOfEntries) {
                     data.labels.shift();
                 }
 
-                if (lastTimeStamp) {
-                    playbackTime += Date.now() - lastTimeStamp;
+                if (self.chartData.lastTimeStamp) {
+                    self.chartData.playbackTime += Date.now() - self.chartData.lastTimeStamp;
                 }
 
-                data.labels.push(parseFloat(playbackTime / 1000).toFixed(3));
+                data.labels.push(parseFloat(self.chartData.playbackTime / 1000).toFixed(3));
 
-                lastTimeStamp = Date.now();
+                self.chartData.lastTimeStamp = Date.now();
 
                 for (var i = 0; i < data.datasets.length; i++) {
-                    if (data.datasets[i].data.length > NUMBER_OF_CHART_ENTRIES) {
+                    if (data.datasets[i].data.length > self.chartNumberOfEntries) {
                         data.datasets[i].data.shift();
                     }
                 }
@@ -256,8 +315,24 @@ App.prototype._setupLineChart = function () {
                 self.chart.update();
             }
         }
-    }, CHART_INTERVAL)
+        self._updateChartData();
+    }, self.chartReportingInterval)
+
 }
+
+App.prototype._adjustChartSettings = function () {
+
+    if (!isNaN(parseInt(this.domElements.chart.interval.value))) {
+        this.chartReportingInterval = parseInt(this.domElements.chart.interval.value);
+    }
+
+    if (!isNaN(parseInt(this.domElements.chart.numberOfEntries.value))) {
+        this.chartNumberOfEntries = parseInt(this.domElements.chart.numberOfEntries.value);
+    }
+
+    this._enableChart(this.domElements.chart.enabled.checked);
+}
+
 
 App.prototype._startIntervalHandler = function () {
     var self = this;
@@ -267,27 +342,26 @@ App.prototype._startIntervalHandler = function () {
             var settings = self.player.getSettings();
 
             var currentLatency = parseFloat(self.player.getCurrentLiveLatency(), 10);
-            document.getElementById('latency-tag').innerHTML = currentLatency + ' secs';
+            self.domElements.metrics.latencyTag.innerHTML = currentLatency + ' secs';
 
-            document.getElementById('mindrift-tag').innerHTML = settings.streaming.liveCatchup.minDrift + ' secs';
+            self.domElements.metrics.mindriftTag.innerHTML = settings.streaming.liveCatchup.minDrift + ' secs';
 
             var currentPlaybackRate = self.player.getPlaybackRate();
-            document.getElementById('playbackrate-tag').innerHTML = Math.round(currentPlaybackRate * 100) / 100;
+            self.domElements.metrics.playbackrateTag.innerHTML = Math.round(currentPlaybackRate * 100) / 100;
 
             var currentBuffer = dashMetrics.getCurrentBufferLevel('video');
-            document.getElementById('buffer-tag').innerHTML = currentBuffer + ' secs';
+            self.domElements.metrics.bufferTag.innerHTML = currentBuffer + ' secs';
 
-            document.getElementById('catchup-threshold-tag').innerHTML = settings.streaming.liveCatchup.latencyThreshold + ' secs';
+            self.domElements.metrics.catchupThresholdTag.innerHTML = settings.streaming.liveCatchup.latencyThreshold + ' secs';
 
             var d = new Date();
             var seconds = d.getSeconds();
-            document.querySelector('#sec').innerHTML = (seconds < 10 ? '0' : '') + seconds;
+            self.domElements.metrics.sec.innerHTML = (seconds < 10 ? '0' : '') + seconds;
             var minutes = d.getMinutes();
-            document.querySelector('#min').innerHTML = (minutes < 10 ? '0' : '') + minutes + ':';
+            self.domElements.metrics.min.innerHTML = (minutes < 10 ? '0' : '') + minutes + ':';
         }
 
     }, METRIC_INTERVAL);
-
 }
 
 App.prototype._registerEventHandler = function () {
@@ -304,6 +378,10 @@ App.prototype._registerEventHandler = function () {
     document.getElementById('export-settings-button').addEventListener('click', function () {
         self._exportSettings();
     })
+
+    document.getElementById('chart-settings-button').addEventListener('click', function () {
+        self._adjustChartSettings();
+    })
 }
 
 App.prototype._registerDashEventHandler = function () {
@@ -317,10 +395,10 @@ App.prototype._unregisterDashEventHandler = function () {
 App.prototype._onRepresentationSwitch = function (e) {
     try {
         if (e.mediaType === 'video') {
-            document.getElementById('video-max-index').innerHTML = e.numberOfRepresentations
-            document.getElementById('video-index').innerHTML = e.currentRepresentation.index + 1;
+            this.domElements.metrics.videoMaxIndex.innerHTML = e.numberOfRepresentations
+            this.domElements.metrics.videoIndex.innerHTML = e.currentRepresentation.index + 1;
             var bitrate = Math.round(e.currentRepresentation.bandwidth / 1000);
-            document.getElementById('video-bitrate').innerHTML = bitrate;
+            this.domElements.metrics.videoBitrate.innerHTML = bitrate;
         }
     } catch (e) {
 
