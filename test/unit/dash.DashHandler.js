940d4ee46fb8be96948b4910eb35179a7a6c6c35
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2021-09-15 16:29:38 +0200

    WiP: Unit tests

--- test/unit/dash.DashHandler.js
+++ test/unit/dash.DashHandler.js
@@ -79,7 +79,7 @@ describe('DashHandler', function () {
     });
 
     it('should return ??? when trying to get a media segment with conform representation parameter', () => {
-        const mediaSegment = dashHandler.getSegmentRequestForTime({segmentInfoType: DashConstants.SEGMENT_BASE});
+        const mediaSegment = dashHandler.getSegmentRequestForTime({ segmentInfoType: DashConstants.SEGMENT_BASE });
 
         expect(mediaSegment).to.be.null; // jshint ignore:line
     });
@@ -89,4 +89,81 @@ describe('DashHandler', function () {
 
         expect(mediaSegment).to.be.null; // jshint ignore:line
     });
-});
+
+    describe('getValidSeekTimeCloseToTargetTime()', () => {
+        let dummyRepresentation;
+        let dummyMediaInfo
+
+        beforeEach(() => {
+            dummyRepresentation = {
+                index: 0,
+                adaptation: {
+                    index: 0,
+                    period: {
+                        start: 0,
+                        duration: 30,
+                        index: 0,
+                        mpd: {
+                            manifest: {
+                                Period_asArray: [
+                                    {
+                                        AdaptationSet_asArray: [{
+                                            Representation_asArray: [
+                                                {
+                                                    SegmentTemplate: {
+                                                        SegmentTimeline: {
+                                                            S_asArray: [{
+                                                                t: 0,
+                                                                r: 14,
+                                                                d: 2
+                                                            }]
+                                                        }
+                                                    }
+                                                }
+                                            ]
+                                        }]
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                },
+                segmentInfoType: 'SegmentTimeline',
+                timescale: 1,
+                segmentDuration: 4
+            };
+            dummyMediaInfo = {};
+        })
+
+        it('should return NaN if no parameters are passed', () => {
+            const result = dashHandler.getValidSeekTimeCloseToTargetTime()
+
+            expect(result).to.be.NaN;
+        })
+
+        it('should return NaN if time is not a valid number', () => {
+            const result = dashHandler.getValidSeekTimeCloseToTargetTime('a', {}, {}, 0.5)
+
+            expect(result).to.be.NaN;
+        })
+
+        it('should return NaN if time if invalid mediainfo is passed', () => {
+            const result = dashHandler.getValidSeekTimeCloseToTargetTime(3, null, {}, 0.5)
+
+            expect(result).to.be.NaN;
+        })
+
+        it('should return NaN if time if invalid representation is passed', () => {
+            const result = dashHandler.getValidSeekTimeCloseToTargetTime(3, {}, null, 0.5)
+
+            expect(result).to.be.NaN;
+        })
+
+        it('should return valid time if request can be found', () => {
+            const result = dashHandler.getValidSeekTimeCloseToTargetTime(3, dummyMediaInfo, dummyRepresentation, 0.5)
+
+            expect(result).to.be.NaN;
+        })
+    })
+})
+;
