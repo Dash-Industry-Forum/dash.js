c1f889d6d757e8d61318d011764693681ac01c6a
Chanh Tran <minhchanhbkit@gmail.com>
2021-10-12 23:18:32 +0900

    Fix scheduleWhilePaused not working  (#3785)
    * Fix scheduleWhilePaused not working

--- src/streaming/controllers/StreamController.js
+++ src/streaming/controllers/StreamController.js
@@ -752,11 +752,14 @@ function StreamController() {
      * @private
      */
     function _onPlaybackStarted( /*e*/) {
-        logger.debug('[onPlaybackStarted]');
-        if (!initialPlayback && isPaused) {
-            isPaused = false;
+        logger.debug('[onPlaybackStarted]');        
+        if (!initialPlayback && isPaused) {            
             createPlaylistMetrics(PlayList.RESUME_FROM_PAUSE_START_REASON);
         }
+        if (initialPlayback) {
+            initialPlayback = false;
+        }
+        isPaused = false;
     }
 
     /**
@@ -982,6 +985,22 @@ function StreamController() {
      */
     function getActiveStream() {
         return activeStream;
+    }
+
+    /**
+     * Initial playback indicates if we have called play() for the first time yet.
+     * @return {*}
+     */
+    function getInitialPlayback() {
+        return initialPlayback;
+    }
+
+    /**
+     * Auto Play indicates if the stream starts automatically as soon as it is initialized.
+     * @return {boolean}
+     */
+    function getAutoPlay() {
+        return autoPlay;
     }
 
     /**
@@ -1504,6 +1523,8 @@ function StreamController() {
         getHasMediaOrInitialisationError,
         getStreams,
         getActiveStream,
+        getInitialPlayback,
+        getAutoPlay,
         reset
     };
 
