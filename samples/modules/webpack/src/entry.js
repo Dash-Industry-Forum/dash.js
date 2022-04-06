8b0e78ea93f1ccc61b768527a4f3bdfe0810bdf3
dsilhavy <daniel.silhavy@fokus.fraunhofer.de>
2022-04-06 11:55:43 +0200

    Remove falsely added controlbar

--- samples/modules/webpack/src/entry.js
+++ samples/modules/webpack/src/entry.js
@@ -1,10 +1,6 @@
 import { MediaPlayer } from 'dashjs';
-import ControlBar from 'dashjs/contrib/akamai/controlbar/ControlBar';
 
 let url = "https://dash.akamaized.net/envivio/Envivio-dash2/manifest.mpd";
 let player = MediaPlayer().create();
 player.initialize(document.querySelector('#myMainVideoPlayer'), url, true);
 
-const controlBar = new ControlBar(player)
-
-controlBar.initialize()
