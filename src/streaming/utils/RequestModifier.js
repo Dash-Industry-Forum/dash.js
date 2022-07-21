264943446cd4a8290415143882333a123f90ff23
Łukasz Kinasiewicz <lukasz.kinasiewicz@schange.com>
2022-07-21 15:19:52 +0200

    Pass url to RequestModifier.modifyRequestHeader (#3995)

--- src/streaming/utils/RequestModifier.js
+++ src/streaming/utils/RequestModifier.js
@@ -39,7 +39,8 @@ function RequestModifier() {
         return url;
     }
 
-    function modifyRequestHeader(request) {
+    // eslint-disable-next-line no-unused-vars
+    function modifyRequestHeader(request, {url}) {
         return request;
     }
 
