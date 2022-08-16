2181999263e1ab71554d647706489822691cf33f
Bertrand Berthelot <bertrand.berthelot@orange.com>
2022-08-16 09:33:19 +0200

    Update tXml parser to v4.0.1

--- externals/tXml.js
+++ externals/tXml.js
@@ -1,25 +1,18 @@
-/**
-* The MIT License (MIT)
-*
-* Copyright (c) 2015 Tobias Nickel
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+// ==ClosureCompiler==
+// @output_file_name default.js
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// ==/ClosureCompiler==
+// module.exports = {
+//     parse: parse,
+//     simplify: simplify,
+//     simplifyLostLess: simplifyLostLess,
+//     filter: filter,
+//     stringify: stringify,
+//     toContentString: toContentString,
+//     getElementById: getElementById,
+//     getElementsByClassName: getElementsByClassName,
+//     transformStream: transformStream,
+// };
 
 /**
  * @author: Tobias Nickel
@@ -31,131 +24,139 @@
  * @typedef tNode
  * @property {string} tagName
  * @property {object} attributes
- * @property {tNode|string|number[]} children
+ * @property {(tNode|string)[]} children
  **/
+
+/**
+ * @typedef TParseOptions
+ * @property {number} [pos]
+ * @property {string[]} [noChildNodes]
+ * @property {boolean} [setPos]
+ * @property {boolean} [keepComments]
+ * @property {boolean} [keepWhitespace]
+ * @property {boolean} [simplify]
+ * @property {(a: tNode, b: tNode) => boolean} [filter]
+ */
 
 /**
  * parseXML / html into a DOM Object. with no validation and some failur tolerance
  * @param {string} S your XML to parse
- * @param options {object} all other options:
- * searchId {string} the id of a single element, that should be returned. using this will increase the speed rapidly
- * filter {function} filter method, as you know it from Array.filter. but is goes throw the DOM.
-
- * @return {tNode[]}
+ * @param {TParseOptions} [options]  all other options:
+ * @return {(tNode | string)[]}
  */
-function tXml(S, options) {
-    "use strict";
+ export function parse(S, options) {
+    "txml";
     options = options || {};
-    var attrMatchers = options.attrMatchers || [];
-    var nodesAsArray = options.nodesAsArray || [];
 
     var pos = options.pos || 0;
+    var keepComments = !!options.keepComments;
+    var keepWhitespace = !!options.keepWhitespace
 
     var openBracket = "<";
     var openBracketCC = "<".charCodeAt(0);
     var closeBracket = ">";
     var closeBracketCC = ">".charCodeAt(0);
-    var minus = "-";
     var minusCC = "-".charCodeAt(0);
-    var slash = "/";
     var slashCC = "/".charCodeAt(0);
-    var exclamation = '!';
     var exclamationCC = '!'.charCodeAt(0);
-    var singleQuote = "'";
     var singleQuoteCC = "'".charCodeAt(0);
-    var doubleQuote = '"';
     var doubleQuoteCC = '"'.charCodeAt(0);
-    var openCornerBracket = '[';
     var openCornerBracketCC = '['.charCodeAt(0);
-    var closeCornerBracket = ']';
     var closeCornerBracketCC = ']'.charCodeAt(0);
 
 
     /**
      * parsing a list of entries
      */
-    function parseChildren(parent) {
+    function parseChildren(tagName) {
         var children = [];
         while (S[pos]) {
             if (S.charCodeAt(pos) == openBracketCC) {
                 if (S.charCodeAt(pos + 1) === slashCC) {
+                    var closeStart = pos + 2;
                     pos = S.indexOf(closeBracket, pos);
+
+                    var closeTag = S.substring(closeStart, pos)
+                    if (closeTag.indexOf(tagName) == -1) {
+                        var parsedText = S.substring(0, pos).split('\n');
+                        throw new Error(
+                            'Unexpected close tag\nLine: ' + (parsedText.length - 1) +
+                            '\nColumn: ' + (parsedText[parsedText.length - 1].length + 1) +
+                            '\nChar: ' + S[pos]
+                        );
+                    }
+
                     if (pos + 1) pos += 1
+
                     return children;
                 } else if (S.charCodeAt(pos + 1) === exclamationCC) {
                     if (S.charCodeAt(pos + 2) == minusCC) {
                         //comment support
+                        const startCommentPos = pos;
                         while (pos !== -1 && !(S.charCodeAt(pos) === closeBracketCC && S.charCodeAt(pos - 1) == minusCC && S.charCodeAt(pos - 2) == minusCC && pos != -1)) {
                             pos = S.indexOf(closeBracket, pos + 1);
                         }
                         if (pos === -1) {
                             pos = S.length
                         }
-                    }else if(
-                        S.charCodeAt(pos + 2) === openCornerBracketCC
-                        && S.charCodeAt(pos + 8) === openCornerBracketCC
-                        && S.substr(pos+3, 5).toLowerCase() === 'cdata'
-                    ){
+                        if (keepComments) {
+                            children.push(S.substring(startCommentPos, pos + 1));
+                        }
+                    } else if (
+                        S.charCodeAt(pos + 2) === openCornerBracketCC &&
+                        S.charCodeAt(pos + 8) === openCornerBracketCC &&
+                        S.substr(pos + 3, 5).toLowerCase() === 'cdata'
+                    ) {
                         // cdata
-                        var cdataEndIndex = S.indexOf(']]>',pos)
-                        if (cdataEndIndex==-1) {
-                            children.push(S.substr(pos+8));
-                            pos=S.length;
+                        var cdataEndIndex = S.indexOf(']]>', pos);
+                        if (cdataEndIndex == -1) {
+                            children.push(S.substr(pos + 9));
+                            pos = S.length;
                         } else {
-                            children.push(S.substring(pos+9, cdataEndIndex));
+                            children.push(S.substring(pos + 9, cdataEndIndex));
                             pos = cdataEndIndex + 3;
                         }
-                        continue
+                        continue;
                     } else {
                         // doctypesupport
+                        const startDoctype = pos + 1;
                         pos += 2;
-                        while (S.charCodeAt(pos) !== closeBracketCC && S[pos]) {
+                        var encapsuled = false;
+                        while ((S.charCodeAt(pos) !== closeBracketCC || encapsuled === true) && S[pos]) {
+                            if (S.charCodeAt(pos) === openCornerBracketCC) {
+                                encapsuled = true;
+                            } else if (encapsuled === true && S.charCodeAt(pos) === closeCornerBracketCC) {
+                                encapsuled = false;
+                            }
                             pos++;
                         }
+                        children.push(S.substring(startDoctype, pos));
                     }
                     pos++;
                     continue;
                 }
-                // if parent is provided then add children as object(s)
-                var child = parseNode();
-                if (parent) {
-                    let tagName = child.tagName;
-                    delete child.tagName;
-                    if (nodesAsArray.indexOf(tagName) !== -1) {
-                        if (!parent[tagName]) {
-                            parent[tagName] = [];
-                        }
-                        parent[tagName].push(child);
-                    } else {
-                        parent[tagName] = child;
+                var node = parseNode();
+                children.push(node);
+                if (node.tagName[0] === '?') {
+                    children.push(...node.children);
+                    node.children = [];
+                }
+            } else {
+                var text = parseText();
+                if (keepWhitespace) {
+                    if (text.length > 0) {
+                        children.push(text);
+                    }
+                } else {
+                    var trimmed = text.trim();
+                    if (trimmed.length > 0) {
+                        children.push(trimmed);
                     }
                 }
-                children.push(child);
-            } else {
-                var text = parseText()
-                if (text.trim().length > 0)
-                    if (parent) parent.__text = text;
-                    else children.push(text);
                 pos++;
             }
         }
         return children;
-    }
-
-    function processAttr(tagName, attrName, value) {
-
-        // Specific use case for SegmentTimeline <S> tag
-        if (tagName === 'S') {
-            return parseInt(value);
-        }
-
-        let attrValue = value;
-        attrMatchers.forEach(matcher => {
-            if (matcher.test(tagName, attrName, value)) {
-                attrValue = matcher.converter(value);
-            }
-        });
-        return attrValue;
     }
 
     /**
@@ -169,7 +170,7 @@ function tXml(S, options) {
         return S.slice(start, pos + 1);
     }
     /**
-     *    returns text until the first nonAlphebetic letter
+     *    returns text until the first nonAlphabetic letter
      */
     var nameSpacer = '\r\n\t>/= ';
 
@@ -184,19 +185,13 @@ function tXml(S, options) {
      *    is parsing a node, including tagName, Attributes and its children,
      * to parse children it uses the parseChildren again, that makes the parsing recursive
      */
+    var NoChildNodes = options.noChildNodes || ['img', 'br', 'input', 'meta', 'link', 'hr'];
 
     function parseNode() {
         pos++;
-        let node = {
-            tagName: parseName()
-        };
-
-        // Support tag namespace
-        let p = node.tagName.indexOf(':');
-        if (p !== -1) {
-            node.__prefix = node.tagName.substr(0, p);
-            node.tagName = node.tagName.substr(p + 1);
-        }
+        const tagName = parseName();
+        const attributes = {};
+        let children = [];
 
         // parsing attributes
         while (S.charCodeAt(pos) !== closeBracketCC && S[pos]) {
@@ -213,25 +208,46 @@ function tXml(S, options) {
                 if (code === singleQuoteCC || code === doubleQuoteCC) {
                     var value = parseString();
                     if (pos === -1) {
-                        return node;
+                        return {
+                            tagName,
+                            attributes,
+                            children,
+                        };
                     }
                 } else {
                     value = null;
                     pos--;
                 }
-                value = processAttr(node.tagName, name, value);
-                node[name] = value;
+                attributes[name] = value;
             }
             pos++;
         }
         // optional parsing of children
         if (S.charCodeAt(pos - 1) !== slashCC) {
-            pos++;
-            parseChildren(node);
+            if (tagName == "script") {
+                var start = pos + 1;
+                pos = S.indexOf('</script>', pos);
+                children = [S.slice(start, pos)];
+                pos += 9;
+            } else if (tagName == "style") {
+                var start = pos + 1;
+                pos = S.indexOf('</style>', pos);
+                children = [S.slice(start, pos)];
+                pos += 8;
+            } else if (NoChildNodes.indexOf(tagName) === -1) {
+                pos++;
+                children = parseChildren(tagName);
+            } else {
+                pos++
+            }
         } else {
             pos++;
         }
-        return node;
+        return {
+            tagName,
+            attributes,
+            children,
+        };
     }
 
     /**
@@ -240,7 +256,7 @@ function tXml(S, options) {
 
     function parseString() {
         var startChar = S[pos];
-        var startpos = pos+1;
+        var startpos = pos + 1;
         pos = S.indexOf(startChar, startpos)
         return S.slice(startpos, pos);
     }
@@ -273,7 +289,15 @@ function tXml(S, options) {
     } else if (options.parseNode) {
         out = parseNode()
     } else {
-        out = parseChildren();
+        out = parseChildren('');
+    }
+
+    if (options.filter) {
+        out = filter(out, options.filter);
+    }
+
+    if (options.simplify) {
+        return simplify(Array.isArray(out) ? out : [out]);
     }
 
     if (options.setPos) {
@@ -283,9 +307,177 @@ function tXml(S, options) {
     return out;
 }
 
+/**
+ * transform the DomObject to an object that is like the object of PHP`s simple_xmp_load_*() methods.
+ * this format helps you to write that is more likely to keep your program working, even if there a small changes in the XML schema.
+ * be aware, that it is not possible to reproduce the original xml from a simplified version, because the order of elements is not saved.
+ * therefore your program will be more flexible and easier to read.
+ *
+ * @param {tNode[]} children the childrenList
+ */
+export function simplify(children) {
+    var out = {};
+    if (!children.length) {
+        return '';
+    }
 
-if ('object' === typeof module) {
-    module.exports = tXml;
-    tXml.xml = tXml;
-}
+    if (children.length === 1 && typeof children[0] == 'string') {
+        return children[0];
+    }
+    // map each object
+    children.forEach(function(child) {
+        if (typeof child !== 'object') {
+            return;
+        }
+        if (!out[child.tagName])
+            out[child.tagName] = [];
+        var kids = simplify(child.children);
+        out[child.tagName].push(kids);
+        if (Object.keys(child.attributes).length && typeof kids !== 'string') {
+            kids._attributes = child.attributes;
+        }
+    });
 
+    for (var i in out) {
+        if (out[i].length == 1) {
+            out[i] = out[i][0];
+        }
+    }
+
+    return out;
+};
+
+
+/**
+ * similar to simplify, but lost less
+ *
+ * @param {tNode[]} children the childrenList
+ */
+export function simplifyLostLess(children, parentAttributes = {}) {
+    var out = {};
+    if (!children.length) {
+        return out;
+    }
+
+    if (children.length === 1 && typeof children[0] == 'string') {
+        return Object.keys(parentAttributes).length ? {
+            _attributes: parentAttributes,
+            value: children[0]
+        } : children[0];
+    }
+    // map each object
+    children.forEach(function(child) {
+        if (typeof child !== 'object') {
+            return;
+        }
+        if (!out[child.tagName])
+            out[child.tagName] = [];
+        var kids = simplifyLostLess(child.children || [], child.attributes);
+        out[child.tagName].push(kids);
+        if (Object.keys(child.attributes).length) {
+            kids._attributes = child.attributes;
+        }
+    });
+
+    return out;
+};
+
+/**
+ * behaves the same way as Array.filter, if the filter method return true, the element is in the resultList
+ * @params children{Array} the children of a node
+ * @param f{function} the filter method
+ */
+export function filter(children, f, dept = 0, path = '') {
+    var out = [];
+    children.forEach(function(child, i) {
+        if (typeof(child) === 'object' && f(child, i, dept, path)) out.push(child);
+        if (child.children) {
+            var kids = filter(child.children, f, dept + 1, (path ? path + '.' : '') + i + '.' + child.tagName);
+            out = out.concat(kids);
+        }
+    });
+    return out;
+};
+
+/**
+ * stringify a previously parsed string object.
+ * this is useful,
+ *  1. to remove whitespace
+ * 2. to recreate xml data, with some changed data.
+ * @param {tNode} O the object to Stringify
+ */
+export function stringify(O) {
+    var out = '';
+
+    function writeChildren(O) {
+        if (O) {
+            for (var i = 0; i < O.length; i++) {
+                if (typeof O[i] == 'string') {
+                    out += O[i].trim();
+                } else {
+                    writeNode(O[i]);
+                }
+            }
+        }
+    }
+
+    function writeNode(N) {
+        out += "<" + N.tagName;
+        for (var i in N.attributes) {
+            if (N.attributes[i] === null) {
+                out += ' ' + i;
+            } else if (N.attributes[i].indexOf('"') === -1) {
+                out += ' ' + i + '="' + N.attributes[i].trim() + '"';
+            } else {
+                out += ' ' + i + "='" + N.attributes[i].trim() + "'";
+            }
+        }
+        if (N.tagName[0] === '?') {
+            out += '?>';
+            return;
+        }
+        out += '>';
+        writeChildren(N.children);
+        out += '</' + N.tagName + '>';
+    }
+    writeChildren(O);
+
+    return out;
+};
+
+
+/**
+ * use this method to read the text content, of some node.
+ * It is great if you have mixed content like:
+ * this text has some <b>big</b> text and a <a href=''>link</a>
+ * @return {string}
+ */
+export function toContentString(tDom) {
+    if (Array.isArray(tDom)) {
+        var out = '';
+        tDom.forEach(function(e) {
+            out += ' ' + toContentString(e);
+            out = out.trim();
+        });
+        return out;
+    } else if (typeof tDom === 'object') {
+        return toContentString(tDom.children)
+    } else {
+        return ' ' + tDom;
+    }
+};
+
+export function getElementById(S, id, simplified) {
+    var out = parse(S, {
+        attrValue: id
+    });
+    return simplified ? tXml.simplify(out) : out[0];
+};
+
+export function getElementsByClassName(S, classname, simplified) {
+    const out = parse(S, {
+        attrName: 'class',
+        attrValue: '[a-zA-Z0-9- ]*' + classname + '[a-zA-Z0-9- ]*'
+    });
+    return simplified ? tXml.simplify(out) : out;
+};
