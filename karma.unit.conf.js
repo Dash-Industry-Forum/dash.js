36cbdbae50f9bab206e1e880f15fa9f466cb73d7
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2023-01-03 11:23:27 +0100

    Additional config changes

--- karma.unit.conf.js
+++ karma.unit.conf.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
             'karma-coverage',
             'karma-mocha-reporter',
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
         ],
 
         middleware: [],
@@ -88,7 +89,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadless', 'FirefoxHeadless'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
