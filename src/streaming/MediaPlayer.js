08ead1a7db15c949bc74adfdbba627ff64a700aa
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-08-24 11:34:21 +0100

    style: split line

--- src/streaming/MediaPlayer.js
+++ src/streaming/MediaPlayer.js
@@ -1930,7 +1930,8 @@ function MediaPlayer() {
                 eventBus.off(Events.INTERNAL_MANIFEST_LOADED, handler, self);
 
                 if (e.error) {
-                    return callback(null, e.error);
+                    callback(null, e.error);
+                    return;
                 }
 
                 callback(e.manifest);
