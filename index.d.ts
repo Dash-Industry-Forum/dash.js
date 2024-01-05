b58e2e5169416054fd6747c397460177945741df
Krzysztof Konopko <kris@youview.com>
2024-01-05 13:16:14 +0100

    Fix PlaybackErrorEvent in index.d.ts
    `PlaybackErrorEvent` interface definition is duplicated in `index.d.ts`.
    This commit removes the duplicate.
    
    `PlaybackErrorEvent` is also missing in `ErrorEvent` type where this
    commit adds it.
    
    This commit should not cause any functional changes as it merely updates
    type definitions in a non-breaking manner.

--- index.d.ts
+++ index.d.ts
@@ -1702,6 +1702,7 @@ declare namespace dashjs {
         GenericErrorEvent
         | DownloadErrorEvent
         | ManifestErrorEvent
+        | PlaybackErrorEvent
         | TimedTextErrorEvent
         | MediaPlayerErrorEvent;
 
@@ -1941,11 +1942,6 @@ declare namespace dashjs {
     export interface AdaptationSetRemovedNoCapabilitiesEvent extends Event {
         type: MediaPlayerEvents['ADAPTATION_SET_REMOVED_NO_CAPABILITIES'];
         adaptationSet: object;
-    }
-
-    export interface PlaybackErrorEvent extends Event {
-        type: MediaPlayerEvents['PLAYBACK_ERROR'];
-        error: string;
     }
 
     export interface MediaSettings {
