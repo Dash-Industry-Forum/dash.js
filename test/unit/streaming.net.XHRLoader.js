524b35712a169a15c8f7b5889ef1fcc0feaa3847
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2024-03-12 15:29:46 +0100

    Fix unit tests

--- test/unit/streaming.net.XHRLoader.js
+++ test/unit/streaming.net.XHRLoader.js
@@ -25,107 +25,80 @@ describe('XHRLoader', function () {
     afterEach(function () {
     });
 
-    it('should call success and complete callback when load is called successfully', () => {
+    it('should call onloadend callback when load is called successfully', () => {
         const self = this.ctx;
-        const callbackSucceeded = sinon.spy();
-        const callbackCompleted = sinon.spy();
-        const callbackError = sinon.spy();
+        const callbackLoadend = sinon.spy();
         const callbackAbort = sinon.spy();
 
         xhrLoader = XHRLoader(context).create({});
         const request = {
             request: {},
             customData: {
-                onload: callbackSucceeded,
-                onloadend: callbackCompleted,
-                onerror: callbackError,
+                onloadend: callbackLoadend,
                 onabort: callbackAbort    
             }
         };
         xhrLoader.load(request, {});
         expect(self.requests.length).to.equal(1);
         self.requests[0].respond(200);
-        sinon.assert.notCalled(callbackError);
-        sinon.assert.calledOnce(callbackSucceeded);
-        sinon.assert.calledOnce(callbackCompleted);
+        sinon.assert.calledOnce(callbackLoadend);
         sinon.assert.notCalled(callbackAbort);
-        expect(callbackSucceeded.calledBefore(callbackCompleted)).to.be.true; // jshint ignore:line
     });
 
-    it('should call onload and complete callback when load is called and there is an error response', () => {
+    it('should call onloadend callback when load is called and there is an error response', () => {
         const self = this.ctx;
-        const callbackSucceeded = sinon.spy();
-        const callbackCompleted = sinon.spy();
-        const callbackError = sinon.spy();
+        const callbackLoadend = sinon.spy();
         const callbackAbort = sinon.spy();
         xhrLoader = XHRLoader(context).create({});
         const request = {
             customData: {
-                onload: callbackSucceeded,
-                onloadend: callbackCompleted,
-                onerror: callbackError,
+                onloadend: callbackLoadend,
                 onabort: callbackAbort    
             }
         };
         xhrLoader.load(request, {});
         expect(self.requests.length).to.equal(1);
         self.requests[0].respond(404);
-        sinon.assert.notCalled(callbackError);
-        sinon.assert.calledOnce(callbackCompleted);
-        sinon.assert.calledOnce(callbackSucceeded);
+        sinon.assert.calledOnce(callbackLoadend);
         sinon.assert.notCalled(callbackAbort);
-        expect(callbackSucceeded.calledBefore(callbackCompleted)).to.be.true; // jshint ignore:line
     });
 
     it('should call onabort callback when abort is called', () => {
-        const callbackSucceeded = sinon.spy();
-        const callbackCompleted = sinon.spy();
-        const callbackError = sinon.spy();
+        const callbackLoadend = sinon.spy();
         const callbackAbort = sinon.spy();
 
         xhrLoader = XHRLoader(context).create({});
 
         const request = {
             customData: {
-                onload: callbackSucceeded,
-                onloadend: callbackCompleted,
-                onerror: callbackError,
+                onloadend: callbackLoadend,
                 onabort: callbackAbort    
             }
         };
         xhrLoader.load(request, {});
         xhrLoader.abort();
 
-        sinon.assert.notCalled(callbackError);
-        sinon.assert.notCalled(callbackSucceeded);
         // abort triggers both onloadend and onabort
-        sinon.assert.notCalled(callbackCompleted);
+        sinon.assert.notCalled(callbackLoadend);
         sinon.assert.calledOnce(callbackAbort);
     });
 
-    it('should call onerror and onloadend when load is called and there is a network error', () => {
+    it('should call onloadend when load is called and there is a network error', () => {
         const self = this.ctx;
-        const callbackSucceeded = sinon.spy();
-        const callbackCompleted = sinon.spy();
-        const callbackError = sinon.spy();
+        const callbackLoadend = sinon.spy();
         const callbackAbort = sinon.spy();
         xhrLoader = XHRLoader(context).create({});
         const request = {
             customData: {
-                onload: callbackSucceeded,
-                onloadend: callbackCompleted,
-                onerror: callbackError,
+                onloadend: callbackLoadend,
                 onabort: callbackAbort    
             }
         };
         xhrLoader.load(request, {});
         expect(self.requests.length).to.equal(1);
         self.requests[0].error();
-        sinon.assert.calledOnce(callbackError);
-        sinon.assert.calledOnce(callbackCompleted);
-        sinon.assert.notCalled(callbackSucceeded);
+        sinon.assert.calledOnce(callbackLoadend);
         sinon.assert.notCalled(callbackAbort);
-        expect(callbackError.calledBefore(callbackCompleted)).to.be.true; // jshint ignore:line
     });
 
     it('should set timeout on the sending XHR request', () => {
