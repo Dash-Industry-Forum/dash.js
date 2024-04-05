ed92c4ae9d3f42018399b74d483033dc6aa1619b
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2024-04-05 12:11:22 +0200

    Restructure files for functional tests

--- test/functional/config/test-configurations/full_part_1.conf.js
+++
@@ -1,9 +0,0 @@
-import drm from '../testcases/drm.js'
-import emsg from '../testcases/emsg.js'
-import eptDelta from '../testcases/ept-delta.js'
-import gaps from '../testcases/gaps.js'
-
-export default function getConfig() {
-    return [].concat(drm, emsg, eptDelta, gaps);
-}
-
--- test/functional/config/test-configurations/full_part_3.conf.js
+++
@@ -1,9 +0,0 @@
-import multiperiod from '../testcases/multiperiod.js'
-import subtitle from '../testcases/subtitle.js'
-import vendor from '../testcases/vendor.js'
-import vodBasic from '../testcases/vod-basic.js'
-
-export default function getConfig() {
-    return [].concat(multiperiod, subtitle, vendor, vodBasic);
-}
-
