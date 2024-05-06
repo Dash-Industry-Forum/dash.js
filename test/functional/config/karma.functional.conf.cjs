58c2a7fabbc9bf317f539b2e0884571228c3d757
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2024-05-06 10:52:34 +0200

    Adjust test reports in junit format

--- test/functional/config/karma.functional.conf.cjs
+++ test/functional/config/karma.functional.conf.cjs
@@ -76,8 +76,7 @@ module.exports = function (config) {
             outputDir: `test/functional/results/test/karma/junit`, // results will be saved as $outputDir/$browserName.xml
             outputFile: `${Date.now()}.xml`, // if included, results will be saved as $outputDir/$browserName/$outputFile
             suite: '', // suite will become the package name attribute in xml testsuite element
-            useBrowserName: false, // add browser name to report and classes names
-            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+            useBrowserName: true, // add browser name to report and classes names
             classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
             properties: {}, // key value pair of properties to add to the <properties> section of the report
             xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
