2a5c6590ca1ac4bb7432179a0fe0bb36a6badb10
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2024-05-06 09:47:47 +0200

    Start two browsers at the same time

--- test/functional/config/karma.functional.conf.cjs
+++ test/functional/config/karma.functional.conf.cjs
@@ -144,7 +144,7 @@ module.exports = function (config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: 1
+        concurrency: 4
     })
 }
 
