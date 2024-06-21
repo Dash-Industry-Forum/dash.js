807a0132479f1e7e528dd8663dceb0a89ac35069
Agajan Jumakuliyev <agajan.tm@gmail.com>
2024-06-21 13:09:07 -0700

    Update webpack.config.js
    Configured polyfill for timers to resolve build issues related to xml2js dependency

--- samples/modules/typescript/webpack.config.js
+++ samples/modules/typescript/webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: './src/entry.ts',
     output: {
-        path: './out',
+        path: path.resolve(__dirname, 'out'),
         filename: 'out.js'
     },
     plugins: [
@@ -18,16 +19,25 @@ module.exports = {
         // `node_modules` folder is excluded in order to prevent problems with
         // the library dependencies, as well as `__tests__` folders that
         // contain the tests for the library
-        loaders: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-            exclude: /node_modules/,
-            query: {
-                // we don't want any declaration file in the bundles
-                // folder since it wouldn't be of any use ans the source
-                // map already include everything for debugging
-                declaration: false,
+        // Changed `loaders` to `rules` to be compatible with Webpack 4+
+        rules: [ 
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/,
+                options: { 
+                    // we don't want any declaration file in the bundles
+                    // folder since it wouldn't be of any use ans the source
+                    // map already include everything for debugging
+                    declaration: false,
+                }
             }
-        }]
+        ]
+    },
+    resolve: {
+        fallback: {
+            // need for the timers polyfill
+            timers: require.resolve('timers-browserify') 
+        }
     }
 };
