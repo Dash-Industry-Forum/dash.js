5736987a81d80ad6f1684aac4656ff4e1968f1d1
Agajan Jumakuliyev <agajan.tm@gmail.com>
2024-06-21 13:36:56 -0700

    Update webpack.config.js

--- samples/modules/typescript/webpack.config.js
+++ samples/modules/typescript/webpack.config.js
@@ -1,10 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
     entry: './src/entry.ts',
     output: {
-        path: path.resolve(__dirname, 'out'),
+        path: './out',
         filename: 'out.js'
     },
     plugins: [
@@ -19,25 +18,16 @@ module.exports = {
         // `node_modules` folder is excluded in order to prevent problems with
         // the library dependencies, as well as `__tests__` folders that
         // contain the tests for the library
-        // Changed `loaders` to `rules` to be compatible with Webpack 4+
-        rules: [ 
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/,
-                options: { 
-                    // we don't want any declaration file in the bundles
-                    // folder since it wouldn't be of any use ans the source
-                    // map already include everything for debugging
-                    declaration: false,
-                }
+        loaders: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+            exclude: /node_modules/,
+            query: {
+                // we don't want any declaration file in the bundles
+                // folder since it wouldn't be of any use ans the source
+                // map already include everything for debugging
+                declaration: false,
             }
-        ]
-    },
-    resolve: {
-        fallback: {
-            // need for the timers polyfill
-            timers: require.resolve('timers-browserify') 
-        }
+        }]
     }
 };
