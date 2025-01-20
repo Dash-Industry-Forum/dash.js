8ba35dbb1983495c214f0adfcae612f20ff9570a
Sebastian Piquerez <sebastianp@qualabs.com>
2025-01-20 14:58:50 -0300

    Fix alternative player don't return

--- src/streaming/controllers/AlternativeMpdController.js
+++ src/streaming/controllers/AlternativeMpdController.js
@@ -239,6 +239,7 @@ function AlternativeMpdController() {
         // Initialize alternative player
         if (event != bufferedEvent) {
             _initializeAlternativePlayer(event);
+            _startDashEventPlaybackTimeMonitoring();
         }
     }
 
