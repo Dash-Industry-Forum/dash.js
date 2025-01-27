91fc8307c701c7411f396784ce9d0dfdcf825b5d
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2025-01-27 12:04:04 +0100

    Fix/buffer error tests (#4676)
    * Harmonize after function in functional tests
    
    * Add CJS UMD example for typescript

---
+++ samples/modules/typescript-smooth-umd-cjs/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        inject: true,
+        filename: 'index.html',
+        template: 'index.html'
+    })],
+};
