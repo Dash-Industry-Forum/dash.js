b49512c08e9584edd9cf5df0e52915f5d41c52f3
Evans Owino <9787512+eowino@users.noreply.github.com>
2025-02-13 12:58:34 +0000

    Fix/doris 2781 seek to start date (#59)
    * fix: remove IE11 patch as it prevents seekTo startDate
    
    * 4.7.7

--- src/streaming/models/VideoModel.js
+++ src/streaming/models/VideoModel.js
@@ -104,13 +104,6 @@ function VideoModel() {
                     return;
                 }
 
-                // We don't set the same currentTime because it can cause firing unexpected Pause event in IE11
-                // providing playbackRate property equals to zero.
-                if (element.currentTime === _currentTime) {
-                    _currentTime = NaN;
-                    return;
-                }
-
                 // TODO Despite the fact that MediaSource 'open' event has been fired IE11 cannot set videoElement.currentTime
                 // immediately (it throws InvalidStateError). It seems that this is related to videoElement.readyState property
                 // Initially it is 0, but soon after 'open' event it goes to 1 and setting currentTime is allowed. Chrome allows to
