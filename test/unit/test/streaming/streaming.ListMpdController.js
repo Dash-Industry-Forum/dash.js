24ec616c599cbbce7eb846d1de83826499f7df32
Constanza Dibueno <constanzad@qualabs.com>
2025-02-28 10:58:24 -0300

    basic test case for debugging

--- test/unit/test/streaming/streaming.ListMpdController.js
+++ test/unit/test/streaming/streaming.ListMpdController.js
@@ -1,38 +1,60 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import ListMpdController from '../../../../src/streaming/controllers/ListMpdController.js';
 import EventBus from '../../../../src/core/EventBus.js';
 import Events from '../../../../src/core/events/Events.js';
-import MediaPlayerEvents from '../../../../src/streaming/MediaPlayerEvents.js';
 import ManifestLoaderMock from '../../mocks/ManifestLoaderMock.js';
-import DashAdapter from '../../../../src/dash/DashAdapter.js';
-import DashConstants from '../../../../src/dash/constants/DashConstants.js';
-import DashParser from '../../../../src/dash/parser/DashParser.js';
-import FileLoader from '../../helpers/FileLoader.js';
-import DebugMock from '../../mocks/DebugMock.js';
+import AdapterMock from '../../mocks/AdapterMock.js';
+import VoHelper from '../../helpers/VOHelper.js';
+import Settings from '../../../../src/core/Settings.js';
 
 describe('ListMpdController', function () {
-    let listMpdController, eventBus, manifestLoader, dashAdapter;
+    let listMpdController, eventBus, linkedPeriods;
 
     beforeEach(() => {
-        debugger;
         eventBus = EventBus().getInstance();
         manifestLoader = new ManifestLoaderMock();
         listMpdController = ListMpdController().getInstance();
+        const dashAdapter = new AdapterMock();
+        let settings = Settings().getInstance();
+        let config = {
+            settings: settings,
+            dashAdapter: dashAdapter
+        };
+        listMpdController.setConfig(config);
     });
 
     afterEach(() => {
-        sinon.restore();
     });
 
-    it('should throw an error if the first period does not start at 0', async () => {
-        debugger;
-        let manifest = await FileLoader.loadTextFile('../data/dash/manifest_list_mpd.xml');
-        console.log(manifest)
-        let dashParser = DashParser(context).create({debug: new DebugMock()});
-        let parsedMpd = dashParser.parse(manifest);
-        expect(() => listMpdController.initialize()).to.throw('The first period in a list MPD must have start time equal to 0');
-    });
-
+    it('should throw an error if the first period does not start at 0', () => {
+        let errorCaught
+        let voHelper = new VoHelper();
+        let manifest = voHelper.getDummyMpd();
+        manifest.manifest.Period[0].start = 1
+        listMpdController.initialize();
+        linkedPeriods = [];
+        eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } ).finally(error =>{
+            debugger;
+            errorCaught = error;
+            expect(errorCaught).to.be.an('error');
+        })
     
+        expect(errorCaught.message).to.equal('error');
+        // expect(() => eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )).to.throw('The first period in a list MPD must have start time equal to 0');
+    });
+
+    // it('should correctly set the start time of the first period to 0', () => {
+    // });
+
+    // it('should trigger MANIFEST_UPDATED if no period starts at 0', () => {
+    // });
+
+    // it('should load linked periods when playback time updates', () => {
+    // });
+
+    // it('should correctly determine whether to load a linked period', () => {
+    // });
+
+    // it('should reset the linked period list', () => {
+    // });
 });
