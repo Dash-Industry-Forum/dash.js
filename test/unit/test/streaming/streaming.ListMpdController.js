bf5016ba7a1f6e109d6e8c1e0e4c78df80e22fa3
Constanza Dibueno <constanzad@qualabs.com>
2025-02-28 19:11:06 -0300

    regular periods trigger manifest update event test case WIP

--- test/unit/test/streaming/streaming.ListMpdController.js
+++ test/unit/test/streaming/streaming.ListMpdController.js
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 import ListMpdController from '../../../../src/streaming/controllers/ListMpdController.js';
 import EventBus from '../../../../src/core/EventBus.js';
 import Events from '../../../../src/core/events/Events.js';
@@ -24,35 +24,25 @@ describe('ListMpdController', function () {
     afterEach(() => {
     });
 
-    it('should throw an error if the first period does not start at 0', () => {
-        let errorCaught
+    // it('should throw an error if the first period does not start at 0', async () => {
+    //     let voHelper = new VoHelper();
+    //     let manifest = voHelper.getDummyMpd();
+    //     manifest.manifest.Period[0].start = 1
+    //     listMpdController.initialize();
+    //     const consoleErrorSpy = sinon.spy(console, "error");
+    //     linkedPeriods = [];
+    //     eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
+    //     expect(consoleErrorSpy.calledOnce).to.be.true;
+    //     expect(consoleErrorSpy.firstCall.args[0].message).to.equal("The first period in a list MPD must have start time equal to 0");
+    // });
+
+    it("should trigger a manifest update event if the first period is a regular period", () => {
         let voHelper = new VoHelper();
         let manifest = voHelper.getDummyMpd();
-        manifest.manifest.Period[0].start = 1
+        //let manifest = voHelper.getDummyListMpd();
         listMpdController.initialize();
         linkedPeriods = [];
-        eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } ).finally(error =>{
-            debugger;
-            errorCaught = error;
-            expect(errorCaught).to.be.an('error');
-        })
-    
-        expect(errorCaught.message).to.equal('error');
-        // expect(() => eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )).to.throw('The first period in a list MPD must have start time equal to 0');
-    });
-
-    // it('should correctly set the start time of the first period to 0', () => {
-    // });
-
-    // it('should trigger MANIFEST_UPDATED if no period starts at 0', () => {
-    // });
-
-    // it('should load linked periods when playback time updates', () => {
-    // });
-
-    // it('should correctly determine whether to load a linked period', () => {
-    // });
-
-    // it('should reset the linked period list', () => {
-    // });
+        eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
+        //catch event trigger
+    })
 });
