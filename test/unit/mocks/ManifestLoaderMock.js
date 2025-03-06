70f3981eda0ac62b6831d0d8bf8e81bbdf4ff636
Sebastian Piquerez <sebastianp@qualabs.com>
2025-03-06 16:07:52 -0300

    Implement loadLinkedPeriod unit tests

--- test/unit/mocks/ManifestLoaderMock.js
+++ test/unit/mocks/ManifestLoaderMock.js
@@ -7,12 +7,15 @@ function ManifestLoaderMock(responseMock) {
     this.responseMock = responseMock;
 
     this.load = function (url, serviceLocation, queryParams, linkPeriod) {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             this.url = url;
             this.serviceLocation = serviceLocation;
             this.queryParams = queryParams;
             this.linkPeriod = linkPeriod;
             this.loadManifest = true;
+            if (this.responseMock === 'fail') {
+                reject(this.responseMock)
+            }
             resolve(this.responseMock);
         });
     };
--- test/unit/test/streaming/streaming.ListMpdController.js
+++ test/unit/test/streaming/streaming.ListMpdController.js
@@ -11,100 +11,186 @@ import ManifestLoaderMock from '../../mocks/ManifestLoaderMock.js';
 
 
 describe('ListMpdController', function () {
-    let listMpdController, eventBus;
-    let context;
-    let mpdHelper;
-    let manifestLoaderMock;
+    let listMpdController,
+        eventBus,
+        context,
+        mpdHelper,
+        manifestLoaderMock,
+        settings,
+        dashAdapterMock
 
     beforeEach(() => {
         context = {};
         eventBus = EventBus(context).getInstance();
         listMpdController = ListMpdController(context).getInstance();
 
-        const dashAdapterMock = DashAdapter(context).getInstance();
+        dashAdapterMock = DashAdapter(context).getInstance();
         const errorHandlerMock = new ErrorHandlerMock();
         dashAdapterMock.setConfig({
             errHandler: errorHandlerMock,
         });
 
-        let settings = Settings(context).getInstance();
+        settings = Settings(context).getInstance();
 
         mpdHelper = new MpdHelper();
-        let responseMpdMock = mpdHelper.getMpd('static', undefined);
-        responseMpdMock.minBufferTime = 4;
-        manifestLoaderMock = new ManifestLoaderMock(responseMpdMock);
-        let config = {
-            settings: settings,
-            dashAdapter: dashAdapterMock,
-            manifestLoader: manifestLoaderMock
-        };
-
-        listMpdController.setConfig(config);
     });
 
-    afterEach(() => {
-    });
+    describe('IMPORTED_MPDS_LOADED event', function () {
+        beforeEach(() => {
+            const responseMpdMock = mpdHelper.getMpd('static', undefined);
+            responseMpdMock.minBufferTime = 4;
+            manifestLoaderMock = new ManifestLoaderMock(responseMpdMock);
+            const config = {
+                settings: settings,
+                dashAdapter: dashAdapterMock,
+                manifestLoader: manifestLoaderMock
+            };
 
-    it('should trigger a manifest update if first period is a linked period', async () => {
+            listMpdController.setConfig(config);
+        });
 
-        const spy = sinon.spy(eventBus, 'trigger');
+        it('should trigger a manifest update if first period is a linked period', async () => {
+            const spy = sinon.spy(eventBus, 'trigger');
 
-        let manifest = mpdHelper.getListMpd();
-        let linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
-        manifest.Period[0] = linkedPeriod;
-        let regularPeriod = mpdHelper.composePeriod(undefined);
-        manifest.Period[1] = regularPeriod;
-        listMpdController.initialize();
-        let linkedPeriods = [linkedPeriod];
-        let updatedManifest;
-
-        // Create a promise that resolves when MANIFEST_UPDATED is triggered
-        const manifestUpdatedPromise = new Promise((resolve) => {
-            eventBus.on(Events.MANIFEST_UPDATED, (manifest) => {
-                updatedManifest = manifest;
-                resolve();
+            const manifest = mpdHelper.getListMpd();
+            const linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
+            const regularPeriod = mpdHelper.composePeriod(undefined);
+            manifest.Period = [linkedPeriod, regularPeriod];
+            listMpdController.initialize();
+            const linkedPeriods = [linkedPeriod];
+            
+            // Create a promise that resolves when MANIFEST_UPDATED is triggered
+            let updatedManifest;
+            const manifestUpdatedPromise = new Promise((resolve) => {
+                eventBus.on(Events.MANIFEST_UPDATED, (manifest) => {
+                    updatedManifest = manifest;
+                    resolve();
+                });
             });
+
+            // Create a timeout promise that rejects after 4 seconds
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
+            );
+
+            eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
+
+            // Wait for the specific event to occur
+            await Promise.race([manifestUpdatedPromise, timeoutPromise]);
+            sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
+            expect(updatedManifest.manifest.Period[0]).to.have.property('AdaptationSet');
         });
 
-        // Create a timeout promise that rejects after 4 seconds
-        const timeoutPromise = new Promise((_, reject) =>
-            setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
-        );
+        it('should trigger a manifest update event if the first period is a regular period', async () => {
+            const spy = sinon.spy(eventBus, 'trigger');
+            
+            const manifest = mpdHelper.getListMpd();
+            const regularPeriod = mpdHelper.composePeriod(undefined);
+            const linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
+            manifest.Period = [regularPeriod, linkedPeriod];
+            listMpdController.initialize();
+            const linkedPeriods = [linkedPeriod];
 
-        eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
+            // Create a promise that resolves when MANIFEST_UPDATED is triggered
+            const manifestUpdatedPromise = new Promise((resolve) => {
+                eventBus.on(Events.MANIFEST_UPDATED, (resolve));
+            });
 
-        // Wait for the specific event to occur
-        await Promise.race([manifestUpdatedPromise, timeoutPromise]);
-        sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
-        expect(updatedManifest.manifest.Period[0]).to.have.property('AdaptationSet');
-    });
+            // Create a timeout promise that rejects after 4 seconds
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
+            );
 
-    it('should trigger a manifest update event if the first period is a regular period', async () => {
-        const spy = sinon.spy(eventBus, 'trigger');
-        
-        let manifest = mpdHelper.getListMpd();
-        let regularPeriod = mpdHelper.composePeriod(undefined);
-        manifest.Period[0] = regularPeriod;
-        let linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
-        manifest.Period[1] = linkedPeriod;
-        listMpdController.initialize();
-        let linkedPeriods = [linkedPeriod];
+            eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
 
-        // Create a promise that resolves when MANIFEST_UPDATED is triggered
-        const manifestUpdatedPromise = new Promise((resolve) => {
-            eventBus.on(Events.MANIFEST_UPDATED, (resolve));
-        });
-
-        // Create a timeout promise that rejects after 4 seconds
-        const timeoutPromise = new Promise((_, reject) =>
-            setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
-        );
-
-        eventBus.trigger(Events.IMPORTED_MPDS_LOADED, { manifest, linkedPeriods } )
-
-        // Wait for the specific event to occur
-        await Promise.race([manifestUpdatedPromise, timeoutPromise]);
-        sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
+            // Wait for the specific event to occur
+            await Promise.race([manifestUpdatedPromise, timeoutPromise]);
+            sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
+        })
     })
 
+    describe('loadLinkedPeriod', function () {
+        afterEach(() => {
+            listMpdController.reset();
+        });
+        
+        it('should merge a the imported manifest if the load success', async () => {
+            const responseMpdMock = mpdHelper.getMpd('static', undefined);
+            responseMpdMock.minBufferTime = 4;
+            manifestLoaderMock = new ManifestLoaderMock(responseMpdMock);
+            const config = {
+                settings: settings,
+                dashAdapter: dashAdapterMock,
+                manifestLoader: manifestLoaderMock
+            };
+
+            listMpdController.setConfig(config);
+
+            const spy = sinon.spy(eventBus, 'trigger');
+            const manifest = mpdHelper.getListMpd();
+            const regularPeriod = mpdHelper.composePeriod(undefined);
+            const linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
+            manifest.Period = [regularPeriod, linkedPeriod];
+            listMpdController.initialize();
+            
+            // Create a promise that resolves when MANIFEST_UPDATED is triggered
+            let updatedManifest;
+            const manifestUpdatedPromise = new Promise((resolve) => {
+                eventBus.on(Events.MANIFEST_UPDATED, (manifest) => {
+                    updatedManifest = manifest;
+                    resolve();
+                });
+            });
+            // Create a timeout promise that rejects after 4 seconds
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
+            );
+
+            listMpdController.loadLinkedPeriod(manifest, linkedPeriod);
+
+            // Wait for the specific event to occur
+            await Promise.race([manifestUpdatedPromise, timeoutPromise]);
+            sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
+            expect(updatedManifest.manifest.Period[1]).to.have.property('AdaptationSet');
+        });
+
+        it('should not merge a the imported manifest if the load success', async () => {
+            manifestLoaderMock = new ManifestLoaderMock('fail');
+            const config = {
+                settings: settings,
+                dashAdapter: dashAdapterMock,
+                manifestLoader: manifestLoaderMock
+            };
+
+            listMpdController.setConfig(config);
+            const spy = sinon.spy(eventBus, 'trigger');
+            const manifest = mpdHelper.getListMpd();
+            const regularPeriod = mpdHelper.composePeriod(undefined);
+            const linkedPeriod = mpdHelper.composeLinkedPeriod('0', 0);
+            manifest.Period = [regularPeriod, linkedPeriod];
+            listMpdController.initialize();
+            
+            // Create a promise that resolves when MANIFEST_UPDATED is triggered
+            let updatedManifest;
+            const manifestUpdatedPromise = new Promise((resolve) => {
+                eventBus.on(Events.MANIFEST_UPDATED, (manifest) => {
+                    updatedManifest = manifest;
+                    resolve();
+                });
+            });
+
+            // Create a timeout promise that rejects after 4 seconds
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(() => reject(new Error('Timeout: MANIFEST_UPDATED event was not triggered')), 4000)
+            );
+
+            listMpdController.loadLinkedPeriod(manifest, linkedPeriod);
+
+            // Wait for the specific event to occur
+            await Promise.race([manifestUpdatedPromise, timeoutPromise]);
+            sinon.assert.calledWith(spy, Events.MANIFEST_UPDATED, sinon.match.any);
+            expect(updatedManifest.manifest.Period[1]).to.be.undefined;
+
+        });
+    });
 });
