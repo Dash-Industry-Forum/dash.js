7aa94285dcc929fe83f205bbf53018f712fa5333
Ryan McCartney <ryan.mccartney@bbc.co.uk>
2025-07-28 13:23:38 +0100

    Add handleVideoFramesNotAdvancing types

--- index.d.ts
+++ index.d.ts
@@ -989,6 +989,7 @@ declare namespace dashjs {
                 lowLatencyStallThreshold?: number,
                 useAppendWindow?: boolean,
                 setStallState?: boolean
+                handleVideoFramesNotAdvancing?: boolean
                 avoidCurrentTimeRangePruning?: boolean
                 useChangeTypeForTrackSwitch?: boolean
                 mediaSourceDurationInfinity?: boolean
