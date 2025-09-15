5d4ca6f8daf8d1ff08c9317a8c67be96c64ef491
Sebastian Piquerez <seba.piquerez@gmail.com>
2025-09-15 16:45:48 -0300

    refactor: Enhance MediaManagerMock with improved event handling and reset logic

--- test/unit/mocks/MediaManagerMock.js
+++ test/unit/mocks/MediaManagerMock.js
@@ -15,9 +15,16 @@ class MediaManagerMock {
         this.logger = null;
         this.hideAlternativePlayerControls = false;
         this.alternativeContext = null;
-        
-        // Create spy for setConfig method only
+
         this.setConfig = sinon.spy();
+
+        this.switchBackToMainContent = sinon.spy((seekTime) => { // eslint-disable-line no-unused-vars
+            this.isSwitching = true;
+            if (this.altPlayer) {
+                this.altPlayer = null;
+            }
+            this.isSwitching = false;
+        });
     }
 
     initialize() {
@@ -44,58 +51,66 @@ class MediaManagerMock {
 
     switchToAlternativeContent(playerId, alternativeMpdUrl, time = 0) { // eslint-disable-line no-unused-vars
         this.isSwitching = true;
-        
-        // Mock alternative player
+
+        this.eventHandlers = new Map();
+
         this.altPlayer = {
             attachView: sinon.spy(),
             seek: sinon.spy(),
             play: sinon.spy(),
             pause: sinon.spy(),
             reset: sinon.spy(),
-            duration: sinon.spy(() => 60), // Mock 60 second duration
+            duration: sinon.spy(() => 60),
             isDynamic: sinon.spy(() => false),
-            on: sinon.spy(),
-            off: sinon.spy()
+            on: sinon.spy((eventName, handler, context) => {
+                this.eventHandlers.set(eventName, { handler, context });
+            }),
+            off: sinon.spy(),
+            triggerTimeUpdate: (time) => {
+                const eventData = this.eventHandlers.get('playbackTimeUpdated');
+                if (eventData) {
+                    eventData.handler.call(eventData.context, { time });
+                }
+            }
         };
-        
+
+
         this.isSwitching = false;
     }
 
-    switchBackToMainContent(seekTime) { // eslint-disable-line no-unused-vars
-        this.isSwitching = true;
-        
-        if (this.altPlayer) {
-            this.altPlayer = null;
-        }
-        
-        this.isSwitching = false;
-    }
 
     getAlternativePlayer() {
         return this.altPlayer;
     }
 
+    setAlternativePlayer(player) {
+        this.altPlayer = player;
+    }
+
     reset() {
         this.isSwitching = false;
-        this.altPlayer = null;
+
+        if (this.altPlayer) {
+            if (this.altPlayer.off && typeof this.altPlayer.off === 'function') {
+                this.altPlayer.off();
+            }
+            this.altPlayer = null;
+        }
+
         this.prebufferedPlayers.clear();
-    }
+        if (this.eventHandlers) {
+            this.eventHandlers.clear();
+        }
 
-    // Utility method to check if switching is in progress
-    isSwitchingInProgress() {
-        return this.isSwitching;
-    }
+        if (this.switchBackToMainContent) {
+            this.switchBackToMainContent.resetHistory();
+        }
+        if (this.setConfig) {
+            this.setConfig.resetHistory();
+        }
 
-    // Utility method to get prebuffered players count
-    getPrebufferedPlayersCount() {
-        return this.prebufferedPlayers.size;
+        this.setup();
     }
-
-    // Utility method to check if player is prebuffered
-    isPlayerPrebuffered(playerId) {
-        return this.prebufferedPlayers.has(playerId);
-    }
-
 }
 
 export default MediaManagerMock;
