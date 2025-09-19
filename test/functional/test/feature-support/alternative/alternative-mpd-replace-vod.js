b1e1deb15c6e9be00c043fa5ccf96767f8e2c0ec
Constanza Dibueno <constanzad@qualabs.com>
2025-09-19 12:47:54 -0300

    replace functional test use getCurrentTime

--- test/functional/test/feature-support/alternative/alternative-mpd-replace-vod.js
+++ test/functional/test/feature-support/alternative/alternative-mpd-replace-vod.js
@@ -44,7 +44,7 @@ Utils.getTestvectorsForTestcase('feature-support/alternative/alternative-mpd-rep
             player.registerEvent(Constants.ALTERNATIVE_MPD.CONTENT_START, (data) => {
                 if (data.event.mode === 'replace') {
                     alternativeContentDetected = true;
-                    alternativeStartTime = Date.now();
+                    alternativeStartTime = player.getCurrentTime();
                     expectedAlternativeDuration = data.event.duration;
                 }
             });
@@ -53,7 +53,7 @@ Utils.getTestvectorsForTestcase('feature-support/alternative/alternative-mpd-rep
             player.registerEvent(Constants.ALTERNATIVE_MPD.CONTENT_END, (data) => {
                 if (data.event.mode === 'replace') {
                     timeAfterSwitch = videoElement.currentTime;
-                    alternativeEndTime = Date.now();
+                    alternativeEndTime = player.getCurrentTime();
                     backToOriginalDetected = true;
                     clearTimeout(timeout);
                     
@@ -64,9 +64,9 @@ Utils.getTestvectorsForTestcase('feature-support/alternative/alternative-mpd-rep
                         expect(backToOriginalDetected).to.be.true;
                         
                         // Verify that alternative content played for its full duration
-                        const actualAlternativeDuration = (alternativeEndTime - alternativeStartTime) / 1000; // Convert to seconds
+                        const actualAlternativeDuration = alternativeEndTime - alternativeStartTime; // Both are in seconds now
                         expect(actualAlternativeDuration).to.be.at.least(expectedAlternativeDuration - 1); // Allow 1 second tolerance
-                        expect(actualAlternativeDuration).to.be.at.most(expectedAlternativeDuration + 1.5); // Allow 1 second tolerance
+                        expect(actualAlternativeDuration).to.be.at.most(expectedAlternativeDuration + 1.5); // Allow 1.5 second tolerance
                         
                         // For REPLACE mode, it expectes to return on presentationTime + duration
                         const expectedMinTime = data.event.presentationTime + data.event.duration;
