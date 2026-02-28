c23978beed74dd2e195fa541f91d9c414e7b5f6d
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2026-02-28 14:53:15 +0100

    Further refactoring of the Fairplay flow

--- src/streaming/protection/vo/MediaCapability.js
+++ src/streaming/protection/vo/MediaCapability.js
@@ -36,16 +36,14 @@ class MediaCapability {
     /**
      * @param {string} contentType MIME type and codecs (RFC6386)
      * @param {string} robustness
-     * @param {string} [encryptionScheme] encryption scheme (e.g. 'cenc', 'cbcs')
+     * @param {string | null} [encryptionScheme] encryption scheme (e.g. 'cenc', 'cbcs')
      * @class
      * @ignore
      */
-    constructor(contentType, robustness, encryptionScheme) {
+    constructor(contentType, robustness, encryptionScheme = null) {
         this.contentType = contentType;
         this.robustness = robustness;
-        if (encryptionScheme) {
-            this.encryptionScheme = encryptionScheme;
-        }
+        this.encryptionScheme = encryptionScheme;
     }
 }
 
