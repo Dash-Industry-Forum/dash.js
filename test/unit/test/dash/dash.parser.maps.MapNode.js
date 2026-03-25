d4afee5799552f46ac1676572452459815eb830e
Stephan Schreiner <92983372+stschr@users.noreply.github.com>
2026-03-25 17:50:31 +0100

    Property mapping while MPD parsing (#4901)
    * enable RegEx for Exceptions
    
    * move local RegEx into Constants
    
    * fixing regression issue with MapNode exceptions
    
    * move code into sub-function
    
    * rename some variables
    
    * remove merge flag form MapNode
    
    * enable RegEx for Exceptions
    
    * move code into sub-function
    
    * rename some variables
    
    * remove merge flag form MapNode
    
    * - reestablish new file lost
    - remove duplicated code
    while merging
    
    * Revert changes to clean history
    
    * add one unit test for an example of a DASH XML element that can be present only once
    
    * rename variables to improve intelligibility of their semantics
    
    * implement merging of attributes for non-array Nodes per DASH spec
    
    * add type check prior to merging values

--- test/unit/test/dash/dash.parser.maps.MapNode.js
+++ test/unit/test/dash/dash.parser.maps.MapNode.js
@@ -1,5 +1,4 @@
 import MapNode from '../../../../src/dash/parser/maps/MapNode.js';
-import CommonProperty from '../../../../src/dash/parser/maps/CommonProperty.js';
 import {expect} from 'chai';
 
 describe('MapNode', function () {
@@ -13,25 +12,16 @@ describe('MapNode', function () {
         expect(mapNode.properties).to.be.empty; // jshint ignore:line
         expect(mapNode.children).to.be.instanceOf(Array); // jshint ignore:line
         expect(mapNode.children).to.be.empty; // jshint ignore:line
+        expect(mapNode.exceptions).to.be.instanceOf(Object); // jshint ignore:line
+        expect(mapNode.exceptions).to.be.empty; // jshint ignore:line
     });
 
     it('should throw an exception if attempting to use setters', () => {
         const mapNode = new MapNode();
 
-        ['name', 'properties', 'children'].forEach(p => {
+        ['name', 'properties', 'children', 'exceptions'].forEach(p => {
             const f = () => mapNode[p] = p;
             expect(f).to.throw(Error);
         });
-    });
-
-    it('should have CommonProperty\'s', () => {
-        const name = '';
-        const properties = ['test'];
-
-        const mapNode = new MapNode(name, properties);
-
-        expect(mapNode.properties).to.be.instanceOf(Array); // jshint ignore:line
-        expect(mapNode.properties).not.to.be.empty; // jshint ignore:line
-        expect(mapNode.properties[0]).to.be.instanceOf(CommonProperty); // jshint ignore:line
     });
 });
