93f6aec340cbecdaee504e28e0d1029461dad6a0
David Evans <5390145+davemevans@users.noreply.github.com>
2026-05-14 14:32:30 +0100

    Fix unreachable code warning in MediaController and update eslint config to prevent future occurences. (#5020)

--- src/streaming/controllers/MediaController.js
+++ src/streaming/controllers/MediaController.js
@@ -541,8 +541,8 @@ function MediaController() {
 
             return (matchLang && matchIndex && matchViewPoint && (matchRole || (track.type === Constants.AUDIO && isTrackActive)) && matchAccessibility && matchAudioChannelConfiguration);
         } catch (e) {
-            return false;
             logger.error(e);
+            return false;
         }
     }
 
