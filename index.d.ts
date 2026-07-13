9b31050dc47a865e39a055372558582328f6b9d5
Robert Bryer <robert.bryer@bbc.co.uk>
2026-07-13 18:29:55 +0100

    Add the remaining AdaptationSets in the manifest to the capability event (#5048)
    * Add the remaining adaptationsets to the filtering event
    
    * Add remainingAdaptationSets to the typescript interface

--- index.d.ts
+++ index.d.ts
@@ -2876,6 +2876,7 @@ export interface CueExitEvent extends MediaPlayerEvent {
 
 export interface AdaptationSetRemovedNoCapabilitiesEvent extends MediaPlayerEvent {
     adaptationSet: object;
+    remainingAdaptationSets: object[];
     type: MediaPlayerEvents['ADAPTATION_SET_REMOVED_NO_CAPABILITIES'];
 }
 
